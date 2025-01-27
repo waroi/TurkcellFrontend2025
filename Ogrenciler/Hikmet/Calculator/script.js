46d3a952e94cb584bbd60169c10b3ac2df8a9d8f
Hikmet Melik Fırat <hikmetmelik@gmail.com>
2025-01-27 19:38:01 +0300

    Refactor division function to use arrow syntax and remove unnecessary file

--- Ogrenciler/Hikmet/Calculator/script.js
+++ Ogrenciler/Hikmet/Calculator/script.js
@@ -8,9 +8,8 @@ function cikarma(sayi1, sayi2) {
 function carpma(sayi1, sayi2) {
 	return sayi1 * sayi2;
 }
-function bolme(sayi1, sayi2) {
-	return sayi1 / sayi2;
-}
+const bolme = (sayi1, sayi2) => sayi1 / sayi2;
+
 while (control === "evet") {
 	let sayi1 = parseInt(prompt("İlk sayıyı giriniz"));
 	let sayi2 = parseInt(prompt("Ikinci sayıyı giriniz"));
