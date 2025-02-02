42ab083a721de6a4c0c3d785f10f38e42f10182b
Mustafa Cenk Aydın <402523@ogr.ktu.edu.tr>
2025-02-02 15:49:39 +0300

    update: orman ve yeni düşmanlar eklendi.  ırklar ve sınıflar düzenlendi.

--- Ogrenciler/Cenk/ders-13/rpg/script.js
+++ Ogrenciler/Cenk/ders-13/rpg/script.js
@@ -1,7 +1,11 @@
 const player_name = prompt("Hoşgeldin maceracı. Bize adını bahşeder misin?");
-const player_race = prompt("Peki hangi ırktansın? \n(1)İnsan\n(2)Elf\n(3)Cüce");
+const player_race = prompt(
+  "Peki hangi ırktansın? \n(1)İnsan(+10 can +10 hasar)\n(2)Elf(+20 hasar)\n(3)Cüce(+20 can)"
+);
 const player_class = parseInt(
-  prompt("Ne iş yaparsın? \n(1)Savaşçı \n(2)Büyücü\n(3)Haydut")
+  prompt(
+    "Sınıfını seç: \n(1)Savaşçı Kılıç-Kalkan(Dengeli hasar) kullanır \n(2)Büyücü Asa(Fazla orantısız hasar) kullanır\n(3)Haydut Yay(Orantısız hasar) kullanır."
+  )
 );
 
 console.log(
@@ -9,14 +13,16 @@ console.log(
 );
 
 let weapons = [
-  {name: "Kılıç-Kalkan", damage: 10},
-  {name: "Asa", damage: 10},
+  {name: "Kılıç-Kalkan", damage: 12},
+  {name: "Asa", damage: 8},
   {name: "Yay", damage: 10},
 ];
 
 const enemies = [
   {name: "Kurt", health: 30, damage: 5},
-  {name: "Haydut", health: 50, damage: 8},
+  {name: "Haydut", health: 50, damage: 9},
+  {name: "Goblin", health: 40, damage: 7},
+  {name: "Troll", health: 70, damage: 15},
 ];
 
 let player_stats = {
@@ -29,6 +35,23 @@ let player_stats = {
   health_potion: 0,
 };
 
+if (player_race === "1") {
+  player_stats.health += 10;
+  player_stats.weapon.damage += 10;
+} else if (player_race === "2") {
+  player_stats.weapon.damage += 20;
+} else if (player_race === "3") {
+  player_stats.health += 20;
+}
+
+let zar_katsayisi = 7;
+if (player_stats.weapon.name === "Kılıç-Kalkan") {
+  zar_katsayisi = 7;
+} else if (player_stats.weapon.name === "Asa") {
+  zar_katsayisi = 15;
+} else if (player_stats.weapon.name === "Yay") {
+  zar_katsayisi = 11;
+}
 const place = [
   {
     name: "Köy",
@@ -38,6 +61,10 @@ const place = [
   {
     name: "Mağara",
     string: "Mağara girişi gördün. Girmek ister misin? \n(1)Gir\n(2)Girme",
+  },
+  {
+    name: "Orman",
+    string: "Ormana denk geldin. Girmek ister misin? \n(1)Gir\n(2)Girme",
   },
 ];
 
@@ -49,42 +76,58 @@ function random_event(place) {
 let enter = 0;
 function tuccar(enter) {
   let dukkan_choice = 0;
-  if (enter === 0) {
+  if (player_stats.money < 10 && enter === 0) {
     dukkan_choice = prompt(
-      `Dükkana hoşgeldin ${player_stats.name}.\n Kesende ${player_stats.money} altın var.\n Ne yapmak istersin?\n(1) Silahını geliştir. (+10 hasar - 10 altın)\n(2) Can iksiri satın al(+10 can -10 altın).\n(3)Kapı şurada. Çık git.`
+      "Dükkanımdan çık git fakir. (1) Ağlayarak çık. (2) Tüccara küfür ederek çık. (3) Tüccarı dövüp çık."
     );
-  } else if (enter === 1) {
+  } else if (player_stats.money < 10 && enter === 1) {
     dukkan_choice = prompt(
-      `Başka isteğin var mıdır ${player_stats.name}?\n Kesende ${player_stats.money} altın var.\n Ne yapmak istersin?\n(1) Silahını geliştir. (+10 hasar - 10 altın)\n(2) Can iksiri satın al(+10 can -10 altın).\n(3)Kapı şurada. Çık git.`
+      "Kesene bereket yolcu. Hadi selametle. (1) Selametle beyim. (2) Bir şey demeden çık git."
     );
-  }
-
-  if (dukkan_choice === "1") {
-    player_stats.money -= 10;
-    player_stats.weapon.damage += 10;
-    console.log(player_stats);
-    tuccar(1);
-  } else if (dukkan_choice === "2") {
-    player_stats.money -= 10;
-    player_stats.health_potion += 1;
-    console.log(player_stats);
-    tuccar(1);
-  } else if (dukkan_choice === "3") {
-    enter = 0;
   } else {
-    tuccar(1);
+    if (enter === 0) {
+      dukkan_choice = prompt(
+        `Dükkana hoşgeldin ${player_stats.name}.\n Kesende ${player_stats.money} altın var.\n Ne yapmak istersin?\n(1) Silahını geliştir. (+10 hasar - 10 altın)\n(2) Can iksiri satın al(+10 can -10 altın).\n(3)Kapı şurada. Çık git.`
+      );
+    } else if (enter === 1) {
+      dukkan_choice = prompt(
+        `Başka isteğin var mıdır ${player_stats.name}?\n Kesende ${player_stats.money} altın var.\n Ne yapmak istersin?\n(1) Silahını geliştir. (+10 hasar - 10 altın)\n(2) Can iksiri satın al(+10 can -10 altın).\n(3)Kapı şurada. Çık git.`
+      );
+    }
+
+    if (dukkan_choice === "1") {
+      player_stats.money -= 10;
+      player_stats.weapon.damage += 10;
+      console.log(player_stats);
+      tuccar(1);
+    } else if (dukkan_choice === "2") {
+      player_stats.money -= 10;
+      player_stats.health_potion += 1;
+      console.log(player_stats);
+      tuccar(1);
+    } else if (dukkan_choice === "3") {
+      enter = 0;
+    } else {
+      tuccar(1);
+    }
   }
 }
 
 function koy() {
   let koy_choice = prompt(
-    "Tüccara mı gidersin yoksa hana mı gidersin?\n(1)Tüccar\n(2)Hana gidip dinlen "
+    "Tüccara mı gidersin yoksa hana mı gidersin?\n(1)Tüccar\n(2)Hana gidip dinlen (+Max can -5 altın) "
   );
   if (koy_choice === "1") {
     tuccar(0);
   } else if (koy_choice === "2") {
-    console.log("Hana girdin ve güzelce dinlendin.");
-    player_stats.health = 100;
+    if (player_stats.money < 5) {
+      console.log("Git ahırda yat beş parasız seni.");
+      console.log("Ahırda bedavaya yattın (+10 can)");
+      player_stats.health += 10;
+    } else {
+      console.log("Hana girdin ve güzelce dinlendin.");
+      player_stats.health = 100;
+    }
   } else {
     koy();
   }
@@ -109,7 +152,7 @@ function fight() {
       `Canın:${player_stats.health}\nDüşmanın canı:${enemy.health}\n(1) Düşmana saldır (2) İksir iç.`
     );
     if (fight_choice === "1") {
-      zar = Math.floor(Math.random() * 7) - 3;
+      zar = Math.floor(Math.random() * zar_katsayisi) - (zar_katsayisi - 1) / 2;
       if (zar !== 0) {
         let final_damage = player_damage + zar;
         enemy.health -= final_damage;
@@ -160,7 +203,19 @@ function magara() {
     fight();
   }
 }
-
+function orman() {
+  let orman_event = Math.random();
+  if (orman_event > 0.5) {
+    let trophy = Math.floor(Math.random() * 30);
+    console.log(
+      `Kulübe buldun. Kulübenin içinde altın kesesi vardı. İçinden ${trophy} altın çıktı.`
+    );
+    player_stats.money += trophy;
+    console.log(`Kesende ${player_stats.money} altın var.`);
+  } else {
+    fight();
+  }
+}
 let dead = false;
 while (dead === false) {
   let olay = random_event(place);
@@ -171,7 +226,10 @@ while (dead === false) {
   } else if (olay.name === "Köy" && player_choice === "2") {
   } else if (olay.name === "Mağara" && player_choice === "1") {
     magara();
-  } else if (olay.name === "Mağara" && player_choice === "1") {
+  } else if (olay.name === "Mağara" && player_choice === "2") {
+  } else if (olay.name === "Orman" && player_choice === "1") {
+    orman();
+  } else if (olay.name === "Orman" && player_choice === "2") {
   } else if (player_choice === "3") {
     dead = true;
   }
@@ -184,3 +242,4 @@ while (dead === false) {
 //? - console.log'lar düzenlenecek ve daha sade ve anlaşılır bir yapı oluşturulacak.
 //? - hikaye akışı daha iyi hale getirilecek, yeni olaylar gibi özellikleri ekleyeceğiz.
 //? - haydutu yenince güç kazanma gibi bir şeyler ekleyebiliriz
+//! - Place ve enemy sayısı artırılacak
