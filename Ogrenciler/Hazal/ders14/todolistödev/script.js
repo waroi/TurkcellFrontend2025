748d66c93d65780c25611fa43158bda0a3722201
Hikmet Melik Fırat <hikmetmelik@gmail.com>
2025-02-04 17:52:25 +0300

    feat: add item numbering and improve todo item editing

--- Ogrenciler/Hazal/ders14/todolistödev/script.js
+++ Ogrenciler/Hazal/ders14/todolistödev/script.js
@@ -8,15 +8,29 @@ document.getElementById("addTodoBtn").onclick = function () {
 	}
 };
 
+function updateNumbers() {
+	const listItems = document.querySelectorAll(".list-group-item");
+	listItems.forEach((item, index) => {
+		const numberSpan = item.querySelector(".item-number");
+		if (numberSpan) {
+			numberSpan.innerText = `${index + 1}.`;
+		}
+	});
+}
+
 function addTodo(todoInput, descInput) {
 	const li = document.createElement("li");
 	li.className =
 		"list-group-item d-flex justify-content-between align-items-center";
 	let header = document.createElement("h2");
+	header.className = "flex-shrink-1";
 	header.innerText = todoInput;
 	let desc = document.createElement("div");
+	desc.className =
+		"d-flex flex-col justify-content-center align-items-center text-center w-50";
 	desc.innerText = descInput;
-
+	const numberSpan = document.createElement("span");
+	numberSpan.className = "item-number me-2";
 	const buttonRow = document.createElement("div");
 	buttonRow.className = "d-flex gap-2";
 
@@ -26,6 +40,7 @@ function addTodo(todoInput, descInput) {
 	deleteBtn.onclick = function () {
 		alert("Bu işlemi gerçekleştirmek istediğinize emin misiniz?");
 		li.remove();
+		updateNumbers();
 	};
 
 	const completeBtn = document.createElement("button");
@@ -46,38 +61,30 @@ function addTodo(todoInput, descInput) {
 			titleInput.type = "text";
 			titleInput.className = "form-control mb-2";
 			titleInput.value = header.innerText;
-
 			const descInput = document.createElement("input");
 			descInput.type = "text";
 			descInput.className = "form-control mb-2";
 			descInput.value = desc.innerText;
-
 			header.replaceWith(titleInput);
 			desc.replaceWith(descInput);
-
 			editBtn.innerText = "Kaydet";
 			editBtn.className = "btn btn-primary btn-sm";
 		} else {
 			const titleInput = li.querySelector("input:first-child");
 			const descInput = li.querySelector("input:last-of-type");
-
 			const newHeader = document.createElement("h2");
 			newHeader.innerText = titleInput.value;
-
 			const newDesc = document.createElement("div");
 			newDesc.innerText = descInput.value;
-
 			titleInput.replaceWith(newHeader);
 			descInput.replaceWith(newDesc);
-
 			header = newHeader;
 			desc = newDesc;
-
 			editBtn.innerText = "Düzenle";
 			editBtn.className = "btn btn-secondary btn-sm";
 		}
 	};
-
+	li.appendChild(numberSpan);
 	li.appendChild(header);
 	li.appendChild(desc);
 	li.appendChild(buttonRow);
@@ -85,4 +92,5 @@ function addTodo(todoInput, descInput) {
 	buttonRow.appendChild(li.appendChild(completeBtn));
 	buttonRow.appendChild(li.appendChild(deleteBtn));
 	document.getElementById("todoList").appendChild(li);
+	updateNumbers();
 }
