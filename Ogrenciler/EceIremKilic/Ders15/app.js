be498086ab7c731497e045a59b9e33c31506a425
eceiremklc <eceiremklc@gmail.com>
2025-02-05 20:00:45 +0300

    add: ders14 and ders15

---
+++ Ogrenciler/EceIremKilic/Ders15/app.js
@@ -0,0 +1,8 @@
+//Local Storage
+
+let userName = 444;
+localStorage.setItem("name", userName);
+let name = Number(localStorage.getItem("name"));
+console.log(name);
+console.log(typeof name);
+document.write(name);
