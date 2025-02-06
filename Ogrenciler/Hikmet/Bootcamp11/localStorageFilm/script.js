1d589e52c06ce6de3ebfdcaf68c84ef518329e8b
Hikmet Melik Fırat <hikmetmelik@gmail.com>
2025-02-06 21:26:11 +0300

    Add localStorage film management functionality with input fields and display

---
+++ Ogrenciler/Hikmet/Bootcamp11/localStorageFilm/script.js
@@ -0,0 +1,104 @@
+function addFilm() {
+	const filmAdi = document.getElementById("filmName").value;
+	const filmYili = document.getElementById("filmYear").value;
+	const filmYonetmen = document.getElementById("filmDirector").value;
+	const filmImg = document.getElementById("filmImage").value;
+
+	const film = {
+		adi: filmAdi,
+		yili: filmYili,
+		yonetmen: filmYonetmen,
+		img: filmImg,
+	};
+
+	let filmler = JSON.parse(localStorage.getItem("filmler")) || [];
+	filmler.push(film);
+	localStorage.setItem("filmler", JSON.stringify(filmler));
+
+	console.log(filmler);
+
+	filmCardEkle();
+}
+
+function filmCardEkle() {
+	const filmler = JSON.parse(localStorage.getItem("filmler")) || [];
+
+	const filmList = document.getElementById("filmList");
+	filmList.innerHTML = "";
+
+	filmler.forEach((film, index) => {
+		const filmCard = document.createElement("div");
+		filmCard.className = "card mb-2";
+
+		const cardBody = document.createElement("div");
+		cardBody.className = "card-body";
+
+		const filmAdi = document.createElement("h5");
+		filmAdi.className = "card-title";
+		filmAdi.innerText = film.adi;
+
+		const filmYili = document.createElement("p");
+		filmYili.className = "card-text";
+		filmYili.innerText = film.yili;
+
+		const filmYonetmen = document.createElement("p");
+		filmYonetmen.className = "card-text";
+		filmYonetmen.innerText = film.yonetmen;
+
+		const filmImg = document.createElement("img");
+		filmImg.alt = film.adi;
+		filmImg.style.width = "100px";
+		filmImg.style.height = "100px";
+		filmImg.src = film.img;
+		filmImg.className = "card-img-top";
+
+		const filmSilmeButton = document.createElement("a");
+		filmSilmeButton.className = "btn btn-danger";
+		filmSilmeButton.text = "Filmi Sil";
+		filmSilmeButton.id = "sil";
+		filmSilmeButton.onclick = () => deleteFilm(index);
+
+		const filmGüncelle = document.createElement("a");
+		filmGüncelle.className = "btn btn-warning";
+		filmGüncelle.text = "Filmi Güncelle";
+		filmGüncelle.id = "guncelle";
+		filmGüncelle.onclick = () => updateFilm(index);
+
+		
+
+		cardBody.appendChild(filmGüncelle);
+		cardBody.appendChild(filmSilmeButton);
+		cardBody.appendChild(filmImg);
+		cardBody.appendChild(filmAdi);
+		cardBody.appendChild(filmYili);
+		cardBody.appendChild(filmYonetmen);
+
+		filmCard.appendChild(cardBody);
+
+		filmList.appendChild(filmCard);
+	});
+}
+
+function updateFilm(index) {
+	let filmler = JSON.parse(localStorage.getItem("filmler")) || [];
+	const film = filmler[index];
+	console.log(film);
+	document.getElementById("filmName").value = film.adi;
+	document.getElementById("filmDirector").value = film.yonetmen;
+	document.getElementById("filmImage").value = film.img;
+	document.getElementById("filmYear").value = film.yili;
+
+	filmler.splice(index, 1);
+	localStorage.setItem("filmler", JSON.stringify(filmler));
+	console.log(filmler);
+	filmCardEkle();
+}
+
+function deleteFilm(index) {
+	let filmler = JSON.parse(localStorage.getItem("filmler")) || [];
+	filmler.splice(index, 1);
+	localStorage.setItem("filmler", JSON.stringify(filmler));
+	console.log(filmler);
+
+	filmCardEkle();
+}
