e3c9eefb6ae5338a0cec3c24071dcf92d49e064f
Hazal Kütküt <hazalktt@gmail.com>
2025-02-15 15:50:19 +0300

    ders18/19

---
+++ Ogrenciler/Hazal/ders18/03-async-await/app.js
@@ -0,0 +1,19 @@
+class Request {
+    async get(url) {
+        const response = await fetch(url)
+        if (!response.ok) throw new Error("Bir Hata Oluştu:" + response.status);
+        const data = await response.json();
+        return data;
+    }
+}
+
+const request = new Request();
+
+request
+    .get("https://jsonplacehholder.typicode.com/albums")
+    .then((albums) => {
+        console.log(albums);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
