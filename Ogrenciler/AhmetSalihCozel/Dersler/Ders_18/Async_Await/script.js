f0de18065c601c92978e1aa35eb181ad18702e83
ahmetsalihcozel <ahmetsalihcozel@gmail.com>
2025-02-16 23:56:07 +0300

    gameWebSite

---
+++ Ogrenciler/AhmetSalihCozel/Dersler/Ders_18/Async_Await/script.js
@@ -0,0 +1,68 @@
+async function getPosts() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!response.ok) {
+    throw new Error('Hata');
+  }
+  const data = await response.json();
+  return data;
+}
+
+var deneme
+
+async function  denemeFunc (){
+  var deneme = await getPosts()
+
+}
+console.log(deneme)
+
+denemeFunc()
+
+//GET POSTU BÖYLE THEN İLE YAZMAK YERİNE BİR FONKSİYON İÇİNDE ASYNC AWAIT KEYWORDU İLE ASENKRON BİR ŞEKİLDE ÇALIŞTIRABİLİRİZ. BU ŞEKİLDE FONKSİYON DIŞINDA İSTEDİĞİMİZ YERDE İSTEDİĞİMİZ İŞLEMİ YAPABİLİRİZ.
+
+// getPosts()
+//   .then((data) => {
+//     posts = data;
+//     // console.log(posts);
+//   })
+//   .catch((err) => console.error(err));
+
+// console.log(posts);
+
+async function fetchAndLogPosts() {
+  const posts = await getPosts();
+
+  const container = document.createElement('div');
+  container.classList.add(
+    'container',
+    'mt-4',
+    'd-flex',
+    'flex-wrap',
+    'justify-content-center'
+  );
+
+  posts.forEach((post) => {
+    const card = document.createElement('div');
+    card.classList.add('card', 'm-2');
+    card.style.width = '18rem';
+
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+
+    const title = document.createElement('h5');
+    title.classList.add('card-title');
+    title.textContent = post.title;
+
+    const bodyText = document.createElement('p');
+    bodyText.classList.add('card-text');
+    bodyText.textContent = post.body;
+
+    cardBody.appendChild(title);
+    cardBody.appendChild(bodyText);
+    card.appendChild(cardBody);
+    container.appendChild(card);
+  });
+
+  document.body.appendChild(container);
+}
+
+fetchAndLogPosts();
---
+++ Ogrenciler/AhmetSalihCozel/Odevler/Odev_2/templates/game.js
@@ -0,0 +1,11 @@
+export class Game {
+    constructor(gameName, gameText, category, releaseDate, imageURL, producer, steamURL){
+        this.gameName = gameName;
+        this.gameText = gameText;
+        this.category = category;
+        this.releaseDate = releaseDate;
+        this.imageURL = imageURL;
+        this.producer = producer;
+        this.steamURL = steamURL
+    }
+}
