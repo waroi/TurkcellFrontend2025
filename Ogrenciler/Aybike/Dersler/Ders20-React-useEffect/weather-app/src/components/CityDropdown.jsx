04464a466c687624a05f57f8794e5f09ba14e89c
Umut Baybece <77975246+Umut501@users.noreply.github.com>
2025-02-18 21:14:13 +0200

    update

--- Ogrenciler/Aybike/Dersler/Ders20-React-useEffect/weather-app/src/components/CityDropdown.jsx
+++ Ogrenciler/Aybike/Dersler/Ders20-React-useEffect/weather-app/src/components/CityDropdown.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { citiesMap } from "../cityData/cities";
-
+import { useState } from "react";
 function CityDropdown() {
+
+	const [selectedCity, setSelectedCity] = useState("");
+
+	const handleChange = (event) => {
+		setSelectedCity(event.target.value);
+		const { name,latitude, longitude } = citiesMap[selectedCity];
+		console.log(name,latitude, longitude);
+	};
+
 	return (
-		<select>
+		<select onChange={handleChange} value={selectedCity}>
 			<option value="">Şehir Seçin</option>
 			{Object.entries(citiesMap).map(([key, city]) => (
 				<option key={key} value={key}>
