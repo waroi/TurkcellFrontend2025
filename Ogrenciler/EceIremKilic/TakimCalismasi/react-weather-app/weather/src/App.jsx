3428d4142c5c52b65fe3c2c3f06a4048ba09e422
haticeozcelik <hatice.ozceelik@hotmail.com>
2025-02-18 21:35:05 +0300

    Update App.jsx

--- Ogrenciler/EceIremKilic/TakimCalismasi/react-weather-app/weather/src/App.jsx
+++ Ogrenciler/EceIremKilic/TakimCalismasi/react-weather-app/weather/src/App.jsx
@@ -17,54 +17,53 @@ function App() {
 
   const fetchData = async () => {
     try {
-    const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${API_KEY}`
       );
       console.log("API Response Status:", response.status);
 
       if (!response.ok) {
         throw new Error(`Hata! HTTP Durumu: ${response.status}`);
       }
-    if (!response.ok) {
-    throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json();
-    setWeather(data);
-    console.log(data); // Tüm hava durumu verilerini konsolda görebilirsin
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      setWeather(data);
+      console.log(data); // Tüm hava durumu verilerini konsolda görebilirsin
     } catch (error) {
-    console.error(error);
+      console.error(error);
     }
-    };
+  };
 
   useEffect(() => {
     fetchData();
-  })
+  }, [date, city]);
 
   return (
     <>
-    <div className="container">
-      <h1>Hava Durumu</h1>
-      <input type="text" 
-      placeholder="Şehir giriniz"
-      value={city}
-      onChange={handleInputChange}
-      />
-      <input type="date"
-      value={date}
-      onChange={handleDate}
-      />
-      <div className="weatherCard">
-      <h2>{weather.name}</h2>
+      <div className="container">
+        <h1>Hava Durumu</h1>
+        <input type="text"
+          placeholder="Şehir giriniz"
+          value={city}
+          onChange={handleInputChange}
+        />
+        <input type="date"
+          value={date}
+          onChange={handleDate}
+        />
+        {weather ? (<div className="weatherCard">
+          <h2>{weather.name}</h2>
           <p>Temperature: {weather.main.temp}°C</p>
           <p>Description: {weather.weather[0].description}</p>
           <p>Feels like : {weather.main.feels_like}°C</p>
           <p>Humidity : {weather.main.humidity}%</p>
           <p>Pressure : {weather.main.pressure}</p>
           <p>Wind Speed : {weather.wind.speed}m/s</p>
+        </div>) : (<p>Loading</p>)}
       </div>
 
-    </div>
-      
     </>
   )
 }
