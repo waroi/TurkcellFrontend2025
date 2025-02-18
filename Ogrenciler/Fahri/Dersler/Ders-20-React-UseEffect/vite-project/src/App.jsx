ac46f30cd3b523e0e4c542799fe1df2d54ab96b3
Fahri Coşkun <fahricosskun@gmail.com>
2025-02-18 15:28:14 +0300

    Hava durumu uygulaması ilk push

--- Ogrenciler/Fahri/Dersler/Ders-20-React-UseEffect/vite-project/src/App.jsx
+++ Ogrenciler/Fahri/Dersler/Ders-20-React-UseEffect/vite-project/src/App.jsx
@@ -1,16 +1,67 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
+const api = {
+  key: "bcb32cf4bb949b61d79d06ce9317df11",
+  url: "https://api.openweathermap.org/data/2.5/weather",
+};
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [city, setCity] = useState("Sakarya");
+  const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
-    console.log("Değişti");
-  }, [count]);
+    if (!city.trim()) {
+      setWeather(null); // Input boşsa hava durumu verisini sıfırla
+      return;
+    }
+    const fetchWeather = async () => {
+      setLoading(true);
+      const response = await fetch(
+        `${api.url}?q=${city}&appid=${api.key}&units=metric`
+      );
+
+      //todo bunu silince sayfadaki veriler gidiyor
+      if (!response.ok) {
+        setWeather(null);
+        return;
+      }
+
+      const result = await response.json();
+      setWeather(result);
+      setLoading(false);
+    };
+    fetchWeather();
+  }, [city]);
+
   return (
-    <>
-      <div onClick={() => setCount(count + 1)}>{count}</div>
-    </>
+    <div className="App">
+      <h1>Weather App</h1>
+      <input
+        type="text"
+        placeholder="Şehir ismi giriniz"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+      {/* aşağıda koşullu rendering yapıyoruz */}
+      {city.trim() === "" ? ( // Eğer input boşsa şehir giriniz yazısını göstersin
+        <p>Şehir giriniz</p>
+      ) : loading ? (
+        <p>Yükleniyor...</p>
+      ) : (
+        weather && ( // weather verisi varsa yazrıdık bilgileri
+          <div>
+            <h2>Şehir: {weather.name}</h2>
+            <p>Sıcaklık: {weather.main.temp}°C</p>
+            <p>Açıklama: {weather.weather[0].description}</p>
+            <p>Hissedilen: {weather.main.feels_like}°C</p>
+            <p>Nem: {weather.main.humidity}%</p>
+            <p>Rüzgar Hızı: {weather.wind.speed} m/s</p>
+          </div>
+        )
+      )}
+    </div>
   );
 }
 
