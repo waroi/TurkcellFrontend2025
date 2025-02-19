0da4d4e0af460667f20baceda49871b2a987e473
greenmagican <egemenaksoz98@gmail.com>
2025-02-19 03:32:44 +0300

    weather app:
    weather app:

---
+++ Ogrenciler/Egemen/weatherAPP/weather-project/src/components/Weather.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Weather = ({name,age}) => {
+  return (
+    <>
+        <div>
+            {name}
+        </div>
+        <div>{age}</div>
+
+
+    </>
+  )
+}
+
+export default Weather
