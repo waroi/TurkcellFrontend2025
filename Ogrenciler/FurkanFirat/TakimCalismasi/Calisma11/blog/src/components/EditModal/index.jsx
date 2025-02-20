6965d7762e899794607607f0d1224eef372859c8
furkan-firat <furkanfirat@live.com>
2025-02-21 01:02:39 +0300

    feature: add edit blog

---
+++ Ogrenciler/FurkanFirat/TakimCalismasi/Calisma11/blog/src/components/EditModal/index.jsx
@@ -0,0 +1,18 @@
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from '../EditForm';
+function EditModal({ show, handleClose, postItem }) {
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Edit Blog</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form handleClose={handleClose} postItem={postItem} />
+      </Modal.Body>
+      <Modal.Footer></Modal.Footer>
+    </Modal>
+  );
+}
+
+export default EditModal;
