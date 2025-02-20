5948bc993e8ad44b29f11c2e3cbf8bf07566a5f5
Ekin Aslan <ekinaslan.js@gmail.com>
2025-02-20 18:34:39 +0300

    Blog App

--- Ogrenciler/MustafaYıldırım/Uygulamalar/Blog/Blog/src/App.jsx
+++ Ogrenciler/MustafaYıldırım/Uygulamalar/Blog/Blog/src/App.jsx
@@ -1,35 +1,112 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, useRef } from "react";
+import Navigation from "./components/Navigation";
+import Modal from "./components/Modal";
+import Blog from "./components/Blog";
+import Database from "./Database";
+import "./style/App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [blogs, setBlogs] = useState([]);
+  const [blog, setBlog] = useState({
+    id: -1,
+    title: "",
+    author: "",
+    content: "",
+    imageUrl: "",
+  });
+  const [action, setAction] = useState({
+    action: () => {},
+    text: "",
+  });
+  const blogRef = useRef(null);
+
+  //* Can't Use Async Function Directly
+  useEffect(() => {
+    (async () => {
+      setBlogs(await Database.getBlogs());
+    })();
+  }, []);
+
+  useEffect(() => {
+    blogRef.current = blog;
+  }, [blog]);
+
+  useEffect(() => {
+    console.log(blogs);
+  }, [blogs]);
+
+  function openModal(blog) {
+    if (blog) {
+      setBlog(blog);
+      setAction({
+        action: editBlog,
+        text: "Edit",
+      });
+    } else {
+      setBlog({
+        id: -1,
+        title: "",
+        author: "",
+        content: "",
+        imageUrl: "",
+      });
+      setAction({
+        action: addBlog,
+        text: "Add",
+      });
+    }
+
+    new window.bootstrap.Modal(document.querySelector(".modal")).show();
+  }
+
+  async function addBlog() {
+    delete blogRef.current.id;
+
+    blogRef.current.publishedAt = new Date().toISOString().split("T")[0];
+
+    setBlogs([
+      ...blogs,
+      { ...blogRef.current, id: (await Database.addBlog(blogRef.current)).id },
+    ]);
+  }
+
+  async function editBlog() {
+    blogRef.current.publishedAt = new Date().toISOString().split("T")[0];
+
+    await Database.editBlog(blogRef.current);
+
+    setBlogs(
+      blogs.map((blog) =>
+        blog.id == blogRef.current.id ? blogRef.current : blog
+      )
+    );
+  }
+
+  async function deleteBlog(id) {
+    if (confirm("Are you sure you want to delete this blog?")) {
+      await Database.deleteBlog(id);
+      setBlogs(blogs.filter((blog) => blog.id != id));
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navigation openModal={() => openModal()} />
+      <Modal blog={blog} setBlog={setBlog} action={action} />
+      <main className="py-5">
+        <div className="container">
+          {blogs.map((blog) => (
+            <Blog
+              key={blog.id}
+              blog={blog}
+              editBlog={() => openModal(blog)}
+              deleteBlog={deleteBlog}
+            />
+          ))}
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
