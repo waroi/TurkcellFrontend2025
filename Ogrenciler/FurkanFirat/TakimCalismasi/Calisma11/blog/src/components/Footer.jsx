7ea3e2a3f61b135919a33549b52b69893cda462c
elinoza <hillcakmak@gmail.com>
2025-02-20 19:53:21 +0300

    some components and db built

---
+++ Ogrenciler/FurkanFirat/TakimCalismasi/Calisma11/blog/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer bg-dark text-light text-center py-3 fixed-bottom">
+      <p>
+        Made with ❤️ by <strong>Furkan & Egemen & Hilal</strong>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
