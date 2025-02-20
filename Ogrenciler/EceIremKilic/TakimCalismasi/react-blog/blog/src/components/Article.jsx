85416369c1cb1c717e4a9367c24d8ed18a87674b
eceiremklc <eceiremklc@gmail.com>
2025-02-21 00:38:21 +0300

    created cards from db

--- Ogrenciler/EceIremKilic/TakimCalismasi/react-blog/blog/src/components/Article.jsx
+++ Ogrenciler/EceIremKilic/TakimCalismasi/react-blog/blog/src/components/Article.jsx
@@ -1,36 +1,34 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { api } from "../core/RequestModel";
+import {
+  getBlogs,
+  updateBlog,
+  postBlog,
+  deleteBlog,
+} from "../core/RequestModel";
 
 const Article = () => {
   const [blogs, setBlogs] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    async function getBlogs() {
-      try {
-        const response = await fetch("http://localhost:3000/blog");
-        const data = await response.json();
-        if (response.ok) {
-          setBlogs(data);
-          return data;
-        }
-      } catch (error) {
-        console.log("Request Model Error: ", error);
-        return null;
-      }
-    }
-    getBlogs();
+    const fetchBlogs = async () => {
+      const data = await getBlogs();
+      setBlogs(data);
+    };
+    fetchBlogs();
   }, []);
   return (
     <section className="articles bg-light mt-5 py-5">
       <div className="container">
         <div className="row mx-auto">
-          <div className="col-lg-4">
-            {blogs.map((blog, index) => (
-              <div className="card rounded-5 border-0 bg-white">
+          {blogs.map((blog, index) => (
+            <div className="col-lg-4" key={blog.blogID}>
+              <div
+                className="card rounded-5 border-0 bg-white"
+              >
                 <img
-                  src="https://mediatrend.mediamarkt.com.tr/wp-content/uploads/2024/04/Yapay-zeka-ve-gunluk-hayat.jpg"
+                  src={blog.blogImage}
                   className="card-img-top rounded-top-5"
                   alt="..."
                 />
@@ -38,13 +36,15 @@ const Article = () => {
                   <div className="row justify-content-between">
                     <div className="col-8 d-flex align-items-center">
                       <img
-                        src="https://media.licdn.com/dms/image/v2/D4D03AQGcnFAMs-OVDg/profile-displayphoto-shrink_200_200/B4DZPbnWuLHUAc-/0/1734556348739?e=1745452800&v=beta&t=rdzgExb4q8liQ27W3zSkuzP4qQiVhUz02imXX2V7TBY"
+                        src={blog.blogAuthorImg}
                         className="p-0 img-fluid rounded-circle avatar me-2"
                         alt=""
                       />
                       <div className="card-post-detail">
-                        <p className="mb-0">By → Ece</p>
-                        <p className="mb-0 text-muted date-text">13 Ağustos</p>
+                        <p className="mb-0">By → {blog.blogAuthorName}</p>
+                        <p className="mb-0 text-muted date-text">
+                          {blog.blogReleaseDate}
+                        </p>
                       </div>
                     </div>
                     <div className="col-4">
@@ -56,16 +56,18 @@ const Article = () => {
                   <h5 className="card-title text-dark fw-bolder mt-3">
                     {blog.blogTitle}
                   </h5>
-                  <p className="card-text text-muted">{blog.blogContent}</p>
+                  <p className="card-text text-muted">
+                    {blog?.blogContent?.split(" ").slice(0, 15).join(" ")}
+                  </p>
                   <div className="d-flex flex-column">
-                    <a href="#" class="btn btn-primary rounded-pill">
+                    <a href="#" className="btn btn-primary rounded-pill">
                       Daha fazlasını oku
                     </a>
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
--- Ogrenciler/EceIremKilic/TakimCalismasi/react-blog/blog/src/core/RequestModel.jsx
+++ Ogrenciler/EceIremKilic/TakimCalismasi/react-blog/blog/src/core/RequestModel.jsx
@@ -1,71 +1,67 @@
-import { useState } from "react";
-const API_URL = "http://localhost:3000/blogs";
+const API_URL = "http://localhost:3000/blog";
 
-export const api = () => {
-  const [blogs, setBlogs] = useState([]);
-  const [error, setError] = useState(null);
-
-  async function getBlogs() {
-    try {
-      const response = await fetch(API_URL);
-      const data = await response.json();
-      if (response.ok) {
-        setBlogs(data);
-        return data;
-      }
-    } catch (error) {
-      console.log("Request Model Error: ", error);
-      return null;
+export async function getBlogs() {
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    if (response.ok) {
+      console.log("dönen data:", data);
+      return data;
     }
+  } catch (error) {
+    console.log("Request Model Error: ", error);
+    return null;
   }
+}
 
-  async function postBlog() {
-    try {
-      const response = await fetch(API_URL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(blog),
-      });
-      const data = await response.json();
-      if (response.ok) {
-        setBlogs([...blogs, data]);
-        return data;
-      }
-    } catch (error) {
-      console.log("Request Model Error: ", error);
-      return null;
+export async function postBlog(blog) {
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(blog),
+    });
+    const data = await response.json();
+    if (response.ok) {
+      return data;
     }
+  } catch (error) {
+    console.log("Request Model Error: ", error);
+    return null;
   }
+}
 
-  async function deleteBlog() {
-    try {
-      const response = await fetch(`${API_URL}/${id}`, {
-        method: "DELETE",
-      });
-    } catch (error) {
-      console.log("Request Model Error: ", error);
-      return null;
+export async function deleteBlog(id) {
+  try {
+    const response = await fetch(`${API_URL}/${id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      return true;
     }
+  } catch (error) {
+    console.log("Request Model Error: ", error);
+    return null;
   }
+}
 
-  async function updateBlog() {
-    try {
-      const response = await fetch(API_URL, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(blog),
-      });
-      const data = await response.json();
-      if (response.ok) {
-        return data;
-      }
-    } catch (error) {
-      console.log("Request Model Error: ", error);
-      return null;
+export async function updateBlog(id, blog) {
+  try {
+    const response = await fetch(`${API_URL}/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(blog),
+    });
+    const data = await response.json();
+    if (response.ok) {
+      return data;
     }
+  } catch (error) {
+    console.log("Request Model Error: ", error);
+    return null;
   }
-};
+}
