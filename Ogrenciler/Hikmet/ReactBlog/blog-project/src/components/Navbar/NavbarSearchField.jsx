0cb6f67613b3ae596eb92862dd85213c4ee7125c
Fahri Coşkun <fahricosskun@gmail.com>
2025-02-21 16:13:51 +0300

    create modal

--- Ogrenciler/Hikmet/ReactBlog/blog-project/src/components/Navbar/NavbarSearchField.jsx
+++ Ogrenciler/Hikmet/ReactBlog/blog-project/src/components/Navbar/NavbarSearchField.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
+import CreateBlogModal from "../CreateBlog/CreateBlogModal"; // Modal bileşenini içe aktar
 
 function NavbarSearchField() {
-	return (
-		<input
-			type="text"
-			className={styles.navbarSearchField}
-			placeholder="Search Blog"
-		/>
-	);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function handleButtonClick() {
+    setIsModalOpen(true);
+  }
+
+  return (
+    <>
+      <button className="badge" onClick={handleButtonClick}>
+        Create Blog
+      </button>
+      {isModalOpen && <CreateBlogModal onClose={() => setIsModalOpen(false)} />}
+    </>
+  );
 }
 
 export default NavbarSearchField;
