6265a4e956e2d0d5bda949096ffb119dd32a0eb8
Sedanur CEYLAN <sdnrcyln2@gmail.com>
2025-02-21 21:27:26 +0300

    github-odev

---
+++ Ogrenciler/SedanurCeylan/github-uygulama/github-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      
+      <Footer />
+    </>
+  )
+}
+
+export default App
