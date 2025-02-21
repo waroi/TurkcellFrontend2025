15f38adac69bc4b38bc4434c7c935abb8ac595f2
Sedanur CEYLAN <sdnrcyln2@gmail.com>
2025-02-21 14:12:39 +0300

    blog-odev

---
+++ Ogrenciler/SedanurCeylan/blog-odev/blog-proje/src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Footer() {
+    return (
+        <footer class="py-3 my-4">
+            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Anasayfa</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Hakkımda</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">İletişim</a></li>
+
+            </ul>
+            <p class="text-center text-body-secondary">© 2025 Turkcell Bootcamp, Beyza Seda Beyza</p>
+        </footer>
+    );
+}
+
+export default Footer;
