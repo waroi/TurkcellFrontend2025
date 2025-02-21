4f450d8f135f008e09aa987138b3dd99db582ea3
Sularada <111848753+Sularada@users.noreply.github.com>
2025-02-21 18:03:59 +0300

    Finished

--- Ogrenciler/Aybike/Dersler/Ders21/blog-project/src/components/organisms/Footer.jsx
+++ Ogrenciler/Aybike/Dersler/Ders21/blog-project/src/components/organisms/Footer.jsx
@@ -1,7 +1,12 @@
 const Footer = () => {
-    return (
-      <></>
-    );
-  };
-  
-  export default Footer;
+  return (
+    <>
+      <h4 className="text-center mt-4  p-2 bg-white border-top border-dark">
+        {" "}
+        @ Travel Blog 🗺️
+      </h4>
+    </>
+  );
+};
+
+export default Footer;
