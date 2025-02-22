975db7e1b15d595aea345cd02ccad71d4e79a435
Ekin Aslan <ekinaslan.js@gmail.com>
2025-02-22 22:10:37 +0300

    Token

--- Ogrenciler/Beyza-Cankurtaran/ödevler/21-02-2025/github-clone/src/util/fetch.js
+++ Ogrenciler/Beyza-Cankurtaran/ödevler/21-02-2025/github-clone/src/util/fetch.js
@@ -1,14 +1,14 @@
 const URL = "https://api.github.com/";
 
 const token =
-  "ub_pat_11AT2PN4A072jaRlOag1Jl_tCTmefp1LfPHXLGyH7LtTYxlxE4Nrq00cnxmpaX6OeYU4XXLB3Drg2f9z2S";
+  "11AT2PN4A072jaRlOag1Jl_tCTmefp1LfPHXLGyH7LtTYxlxE4Nrq00cnxmpaX6OeYU4XXLB3Drg2f9z2S";
 
 export default {
   getUser: async (user) => {
     return await fetch(`${URL}users/${user}`, {
       method: "GET",
       headers: {
-        Authorization: `token ${token}`,
+        Authorization: `token github_pat_${token}`,
       },
     }).then((response) => response.json());
   },
@@ -17,7 +17,7 @@ export default {
     return await fetch(`${URL}users/${user}/repos?per_page=100`, {
       method: "GET",
       headers: {
-        Authorization: `token ${token}`,
+        Authorization: `token github_pat_${token}`,
       },
     }).then((response) => response.json());
   },
