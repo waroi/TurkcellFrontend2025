cdb02e0bcf008b6ce44612be17f0cab3bd19465b
Umut Baybece <77975246+Umut501@users.noreply.github.com>
2025-02-22 19:13:42 +0200

    update

--- Ogrenciler/MustafaYıldırım/Uygulamalar/Github/src/App.jsx
+++ Ogrenciler/MustafaYıldırım/Uygulamalar/Github/src/App.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { getRepos } from "./Services/services";
+import { getRepos, getUser } from "./Services/services";
 import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import RepoGrid from "./components/RepoGrid";
 const App = () => {
   const [repos, setRepos] = useState([]);
-  const [search, setSearch] = useState(""); 
-  const [userSearch, setUserSearch] = useState(""); 
-  const [username, setUsername] = useState(""); 
+  const [user, setUser] = useState([]);
+  const [search, setSearch] = useState("");
+  const [userSearch, setUserSearch] = useState("");
+  const [username, setUsername] = useState("");
 
   useEffect(() => {
     if (username) {
@@ -21,9 +22,18 @@ const App = () => {
     setSearch(e.target.value);
   };
 
+  useEffect(() => {
+    if (username) {
+      getUser(username).then((data) => {
+        console.log("API'den Gelen Veri:", data);
+        setUser(data);
+      });
+    }
+  }, [username]);
+
   const handleSearchSubmit = () => {
     setUsername(userSearch);
-    setSearch(""); 
+    setSearch("");
   };
   const filteredRepos = repos.filter((repo) =>
     repo.name.toLowerCase().includes(search.toLowerCase())
@@ -31,8 +41,19 @@ const App = () => {
   return (
     <>
       <Navbar userSearch={userSearch} setUserSearch={setUserSearch} handleSearchSubmit={handleSearchSubmit} />
+      {user && user.name && (
+        <div className="text-center my-4">
+          <img src={user.avatar_url} alt="Avatar" width="100" />
+          <h2>{user.name}</h2>
+          {user.bio && <p className="text-gray-600 mt-1">{user.bio}</p>}
+        </div>
+      )}
+
       <RepoGrid repos={filteredRepos} handleSearch={handleSearch} search={search} />
+
     </>
   );
 };
 export default App;
+
+
