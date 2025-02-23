f6f5d50a32c89552f633156b73a8f160a308ea36
furkan-firat <furkanfirat@live.com>
2025-02-23 18:06:10 +0300

    feature: add portal

---
+++ Ogrenciler/FurkanFirat/TakimCalismasi/devrep/src/UserCard/Portal.jsx
@@ -0,0 +1,24 @@
+import ReactDOM from 'react-dom';
+import * as Icon from 'react-bootstrap-icons';
+
+export default function Portal({ userData, target }) {
+  return ReactDOM.createPortal(
+    <div className='d-flex justify-content-center align-items-center p-3 w-100'>
+      <ul className='list-group'>
+        <li className='list-group-item d-flex justify-content-between align-items-center'>
+          Followers: {userData.followers}
+          <Icon.PeopleFill className='mx-2' />
+        </li>
+        <li className='list-group-item d-flex justify-content-between align-items-center'>
+          Following: {userData.following}
+          <Icon.PersonFill className='mx-2' />
+        </li>
+        <li className='list-group-item d-flex justify-content-between align-items-center'>
+          Public Repos: {userData.public_repos}
+          <Icon.GlobeAmericas className='mx-2' />
+        </li>
+      </ul>
+    </div>,
+    target
+  );
+}
