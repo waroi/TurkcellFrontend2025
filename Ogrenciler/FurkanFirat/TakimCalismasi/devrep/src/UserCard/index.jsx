c387c2e2d6aff693206405c67757d6587839b88d
ahmetsalihcozel <ahmetsalihcozel@gmail.com>
2025-02-23 18:56:37 +0300

    HopBurdayım

--- Ogrenciler/FurkanFirat/TakimCalismasi/devrep/src/UserCard/index.jsx
+++ Ogrenciler/FurkanFirat/TakimCalismasi/devrep/src/UserCard/index.jsx
@@ -12,9 +12,24 @@ export default function UserCard({ userData }) {
   }, []);
 
   const handleClick = () => {
-    portalTarget.classList[0] === 'bosDiv'
-      ? setPortalTarget(document.querySelector('.portalDiv'))
-      : setPortalTarget(document.querySelector('.bosDiv'));
+    const protalDiv = document.querySelector('.portalDiv');
+    const bosDiv = document.querySelector('.bosDiv');
+    if(portalTarget.classList[0] === 'bosDiv'){
+      bosDiv.style.opacity = "0"
+      protalDiv.style.opacity = "0"
+      setTimeout(()=>{
+        protalDiv.style.opacity = "1"
+        setPortalTarget(protalDiv)
+      },1000)
+    }else{
+      bosDiv.style.opacity = "0"
+      protalDiv.style.opacity = "0"
+      setTimeout(()=>{
+        bosDiv.style.opacity = "1"
+        setPortalTarget(bosDiv)
+      },1000)
+    }
+
   };
 
   return (
@@ -25,11 +40,14 @@ export default function UserCard({ userData }) {
         <p className='text-secondary'>{userData.login}</p>
       </div>
       <div className='border border-1 my-3'></div>
-      <div className='portalDiv cursor-pointer' onClick={handleClick}>
+      <div className='portalDiv d-flex flex-column justify-content-evenly align-items-center p-3 w-100 cursor-pointer'>
+        <button className='btn btn-outline-primary' onClick={handleClick}>Hop Burdayım!</button>
         <Portal userData={userData} target={portalTarget} />
       </div>
       <div className='border border-1 my-3'></div>
-      <div onClick={handleClick} className='bosDiv' ref={bosDivRef}></div>
+      <div  className='bosDiv d-flex flex-column justify-content-evenly align-items-center p-3 w-100 cursor-pointer' ref={bosDivRef}>
+      <button className='btn btn-outline-primary' onClick={handleClick}>Hop Burdayım!</button>
+      </div>
     </div>
   );
 }
