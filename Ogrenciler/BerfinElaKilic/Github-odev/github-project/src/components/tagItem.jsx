5c295ca7ce9ff38b830ac16c46debc570e03c125
elinoza <hillcakmak@gmail.com>
2025-02-24 00:11:37 +0300

    tag colors fixed

--- Ogrenciler/BerfinElaKilic/Github-odev/github-project/src/components/tagItem.jsx
+++ Ogrenciler/BerfinElaKilic/Github-odev/github-project/src/components/tagItem.jsx
@@ -1,14 +1,14 @@
-import Badge from 'react-bootstrap/Badge';
-import Stack from 'react-bootstrap/Stack';
+import Badge from "react-bootstrap/Badge";
+import Stack from "react-bootstrap/Stack";
 
 const TagItem = ({ tag }) => {
-    return (
-        <>
-            <Badge bg="warning" className='me-1' text="dark">
-                {tag}
-            </Badge>
-        </>
-    )
-}
+  return (
+    <>
+      <Badge bg="transparent" className="me-1 tag" text="dark">
+        {tag}
+      </Badge>
+    </>
+  );
+};
 
-export default TagItem
+export default TagItem;
