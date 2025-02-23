ff167e7b85a46f66683281b01f6c79ed1f8f5ce1
furkan-firat <furkanfirat@live.com>
2025-02-23 14:51:16 +0300

    start project devrep

---
+++ Ogrenciler/FurkanFirat/TakimCalismasi/devrep/src/Navbar/index.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav className='navbar navbar-dark bg-dark'>
+      <div className='container-fluid'>
+        <a className='navbar-brand' href='#'>
+          DevRep
+        </a>
+      </div>
+    </nav>
+  );
+}
