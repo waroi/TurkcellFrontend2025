f6e90ef3df3a19ec36d7823dad6dd34911842792
boranaybike <boranaybike.dev@gmail.com>
2025-02-25 00:26:25 +0300

    Update Movie App

---
+++ Ogrenciler/Cenk/ders-23/movie-app/src/components/layout/Header.jsx
@@ -0,0 +1,28 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+export default function Header() {
+  return (
+    
+    <Navbar className="px-5 bg-body-tertiary justify-content-between">
+          <Navbar.Brand href="#home">Movie App</Navbar.Brand>
+      <Form inline>
+        <Row>
+          <Col xs="auto">
+            <Form.Control
+              type="text"
+              placeholder="Search"
+              className=" mr-sm-2"
+            />
+          </Col>
+          <Col xs="auto">
+            <Button type="submit">Submit</Button>
+          </Col>
+        </Row>
+      </Form>
+    </Navbar>
+  );
+}
