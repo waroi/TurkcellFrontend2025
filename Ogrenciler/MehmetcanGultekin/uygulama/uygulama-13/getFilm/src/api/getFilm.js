f535d79c1aba12b47cabaa7aefba84e62306fdf9
Mehmetcan Gültekin <gultekin55mhmt@gmail.com>
2025-02-24 21:02:25 +0300

    edit api key

--- Ogrenciler/MehmetcanGultekin/uygulama/uygulama-13/getFilm/src/api/getFilm.js
+++ Ogrenciler/MehmetcanGultekin/uygulama/uygulama-13/getFilm/src/api/getFilm.js
@@ -1,18 +1,4 @@
-// const api_key = import.meta.env.API_KEY
-// const url = 'https://api.themoviedb.org/3/authentication';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer '+ api_key
-//   }
-// };
-
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
-
+const api_key = import.meta.env.VITE_API_KEY;
 
 export function getAllFilms() {
     const url = 'https://api.themoviedb.org/3/authentication';
@@ -20,7 +6,7 @@ export function getAllFilms() {
         method: 'GET',
         headers: {
           accept: 'application/json',
-          Authorization:  `${import.meta.env.}`,
+          Authorization:  `Bearer ${api_key}`,
         }
       };
       
