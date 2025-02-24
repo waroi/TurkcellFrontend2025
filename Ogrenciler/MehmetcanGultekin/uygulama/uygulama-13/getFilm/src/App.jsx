048349682d8790e92cf24cfe99ff2d5b2fb4c7f1
Sularada <111848753+Sularada@users.noreply.github.com>
2025-02-24 23:35:38 +0300

    update

--- Ogrenciler/MehmetcanGultekin/uygulama/uygulama-13/getFilm/src/App.jsx
+++ Ogrenciler/MehmetcanGultekin/uygulama/uygulama-13/getFilm/src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
-import {getAllFilms} from './api/getFilm'
-import './App.css'
-import { get } from 'immutable'
+import { useState } from "react";
+import { getAllFilms } from "./api/getFilm";
+import "./App.css";
+import { get } from "immutable";
+import MovieCard from "./components/MovieCard/MovieCard";
 
 function App() {
+  const [movies, setMovies] = useState();
   getAllFilms()
+    .then((data) => {
+      setMovies(data.results);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 
   return (
     <>
-      
+      {movies &&
+        movies.map((movie) => {
+          return <MovieCard movie={movie}></MovieCard>;
+        })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Ogrenciler/MehmetcanGultekin/uygulama/uygulama-13/getFilm/src/components/MovieCard/MovieCard.jsx
+++ Ogrenciler/MehmetcanGultekin/uygulama/uygulama-13/getFilm/src/components/MovieCard/MovieCard.jsx
@@ -1,20 +1,26 @@
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
-
-function MovieCard({movie}) {
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import { getFilm } from "../../api/getFilm";
+// const img_url =
+//   "https://image.tmdb.org/t/p/w500/7iMBZzVZtG0oBug4TfqDb9ZxAOa.jpg";
+function MovieCard({ movie }) {
+  const movie_info = getFilm();
+  console.log(movie_info);
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+    <Card style={{ width: "18rem" }}>
+      <Card.Img
+        variant="top"
+        src={`https://image.tmdb.org/t/p/w500/` + movie?.poster_path}
+      />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        <Card.Title>{movie?.original_title}</Card.Title>
+        <Card.Text>{movie?.overview}</Card.Text>
       </Card.Body>
       <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Release Date: {movie?.release_date}</ListGroup.Item>
+        <ListGroup.Item>
+          Vote Average: {movie?.vote_average} / {movie?.vote_count}
+        </ListGroup.Item>
         <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
       </ListGroup>
       <Card.Body>
