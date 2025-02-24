e1314914f5c6203897a03fb743eadaf7e19b8733
Berfin Ela Kılıç <77438033+Elakilic@users.noreply.github.com>
2025-02-24 21:27:26 +0300

    .

--- Ogrenciler/Hatice/Ders23/Movie-Project/src/services/services.jsx
+++ Ogrenciler/Hatice/Ders23/Movie-Project/src/services/services.jsx
@@ -5,17 +5,9 @@ const Services = () => {
     const fetchData = async () => {
       const API_KEY = "a17d098aa83662acc470d974bf21e392";
       const url = `https://api.themoviedb.org/3/authentication?api_key=${API_KEY}`;
-      
-      const options = {
-        method: "GET",
-        headers: {
-          accept: "application/json",
-          Authorization: `Bearer ${API_KEY}`
-        },
-      };
 
       try {
-        const response = await fetch(url, options);
+        const response = await fetch(url);
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
