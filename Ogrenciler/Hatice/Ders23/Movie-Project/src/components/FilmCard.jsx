ba241f8bea6f0ff7690b082814e3b0eb67e68fe0
Berfin Ela Kılıç <77438033+Elakilic@users.noreply.github.com>
2025-02-25 23:24:51 +0300

    update: movie card

--- Ogrenciler/Hatice/Ders23/Movie-Project/src/components/FilmCard.jsx
+++ Ogrenciler/Hatice/Ders23/Movie-Project/src/components/FilmCard.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import React from "react";
+import { Card, Button } from "react-bootstrap";
 
 const FilmCard = ({ movie, onDetailsClick }) => {
   return (
-    <Card style={{ width: '18rem', margin: '1rem' }}>
-      <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+    <Card>
+      <Card.Img
+        variant="top"
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt={movie.title}
+        className="card-img-top"
+      />
       <Card.Body>
-        <Card.Title>{movie.title}</Card.Title>
+        <Card.Title><a
+          href={`https://www.themoviedb.org/movie/${movie.id}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-decoration-none text-dark"
+        >
+          {movie.title}
+        </a></Card.Title>
         <Card.Text>
-          {movie.overview.length > 100 ? `${movie.overview.substring(0, 100)}...` : movie.overview}
+          {movie.overview.length > 100
+            ? `${movie.overview.substring(0, 100)}...`
+            : movie.overview}
         </Card.Text>
-        <Button variant="primary" onClick={() => onDetailsClick(movie.id)}>Detaylar</Button>
+        <Card.Text> {movie.release_date}</Card.Text>
+        <Card.Text> {Math.round(movie.vote_average * 10)}% </Card.Text>
       </Card.Body>
     </Card>
   );
