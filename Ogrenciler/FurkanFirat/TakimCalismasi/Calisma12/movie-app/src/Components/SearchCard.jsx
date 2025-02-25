7d5139ae88fe47e2c04f1a9e0d5fa2be971f18a2
Mustafa Yıldırım <69580632+mustafayildirim57@users.noreply.github.com>
2025-02-25 13:01:25 +0300

    paginations işlemi

--- Ogrenciler/FurkanFirat/TakimCalismasi/Calisma12/movie-app/src/Components/SearchCard.jsx
+++ Ogrenciler/FurkanFirat/TakimCalismasi/Calisma12/movie-app/src/Components/SearchCard.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-export default function SearchCard(Movie) {
+export default function SearchCard({ Movie, imageMovieURL }) {
   return (
     <>
-      <div className="card mb-3" style={{ maxWidth: "540px" }}>
+      <div className="card mb-3 w-100">
         <div className="row g-0">
-          <div className="col-md-4">
-            <img src="..." className="img-fluid rounded-start" alt="..." />
+          <div className="col-md-3">
+            <img
+              src={`${imageMovieURL}/${Movie.poster_path}`}
+              className="img-fluid rounded-start"
+              alt={Movie.original_title}
+            />
           </div>
-          <div className="col-md-8">
+          <div className="col-md-9">
             <div className="card-body">
               <h5 className="card-title">{Movie.original_title}</h5>
               <p className="card-text">{Movie.overview}</p>
