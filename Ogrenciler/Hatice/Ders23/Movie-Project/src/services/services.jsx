74a6c583ca313a1cb1e34447b78e70b717ee9506
Berfin Ela Kılıç <77438033+Elakilic@users.noreply.github.com>
2025-02-25 10:17:42 +0300

    update: oyuncu ve filmler çekildi.

--- Ogrenciler/Hatice/Ders23/Movie-Project/src/services/services.jsx
+++ Ogrenciler/Hatice/Ders23/Movie-Project/src/services/services.jsx
@@ -1,27 +1,36 @@
-import React, { useEffect } from "react";
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_TOKEN =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMTdkMDk4YWE4MzY2MmFjYzQ3MGQ5NzRiZjIxZTM5MiIsIm5iZiI6MTc0MDQxOTkxNC45NDksInN1YiI6IjY3YmNiMzRhMmFmOTcyZDJkOGE0Nzk2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._F_c6JhgjYb8c3EG2tSHOe4mzluAVijL8sCz5fwcK0M";
 
-const Services = () => {
-  useEffect(() => {
-    const fetchData = async () => {
-      const API_KEY = "a17d098aa83662acc470d974bf21e392";
-      const url = `https://api.themoviedb.org/3/authentication?api_key=${API_KEY}`;
-
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  return <div>Fetching data...</div>;
+const headers = {
+  accept: "application/json",
+  Authorization: `Bearer ${API_TOKEN}`,
 };
 
-export default Services;
+export const fetchMovies = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/movie/popular`, {
+      method: "GET",
+      headers,
+    });
+    if (!response.ok) throw new Error("Film verileri alınamadı.");
+    return await response.json();
+  } catch (error) {
+    console.error("Error:", error);
+    return null;
+  }
+};
+
+export const fetchActors = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/person/popular`, {
+      method: "GET",
+      headers,
+    });
+    if (!response.ok) throw new Error("Oyuncu verileri alınamadı.");
+    return await response.json();
+  } catch (error) {
+    console.error("Error:", error);
+    return null;
+  }
+};
