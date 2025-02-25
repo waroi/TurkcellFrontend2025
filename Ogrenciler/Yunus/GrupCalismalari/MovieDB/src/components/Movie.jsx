dcbbe36d9b8f022785ff731ce3294e7112ed3a7e
yunuso-me <yunusorak08@hotmail.com>
2025-02-25 22:20:45 +0300

    feat: add get actors, movies

---
+++ Ogrenciler/Yunus/GrupCalismalari/MovieDB/src/components/Movie.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Movie = ({ movie }) => {
+    return (
+        <div className="col-lg-3 mb-3" key={movie.id}>
+            <div className="card h-100">
+                <img height={450} src={movie.poster_path ? `https://image.tmdb.org/t/p/original/${movie.poster_path}` : 'https://ih1.redbubble.net/image.4905811447.8675/flat,750x,075,f-pad,750x1000,f8f8f8.jpg'} className="card-img-top" alt={movie.original_title} />
+                <div className="card-body d-flex flex-column justify-content-between">
+                    <h5 className="card-title">
+                        {movie.original_title}
+                    </h5>
+                    <p className="card-text">
+                        {movie.overview.split(' ').splice(0, 50).join(' ')}
+                    </p>
+                    <p>
+                        Çıkış Tarihi: {movie.release_date}
+                    </p>
+                    <button type="button" className="btn btn-warning">
+                        IMDB <span className="badge text-bg-secondary">
+                            {(movie.vote_average).toFixed(1)}
+                        </span>
+                    </button>
+                </div>
+                <div className="actors">
+                    {movie.actors?.map((actor) => (
+                        <img
+                            key={actor.id}
+                            src={actor.profile_path ? `https://image.tmdb.org/t/p/original/${actor.profile_path}` : 'https://ih1.redbubble.net/image.4905811447.8675/flat,750x,075,f-pad,750x1000,f8f8f8.jpg'}
+                            alt={actor.name}
+                            title={actor.name}
+                            width="50"
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Movie
