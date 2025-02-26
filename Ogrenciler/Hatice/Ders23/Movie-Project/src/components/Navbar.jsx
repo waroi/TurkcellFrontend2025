8ad08dee609cfdcd699ef5035de9e7da485beaf7
Berfin Ela Kılıç <77438033+Elakilic@users.noreply.github.com>
2025-02-26 16:39:16 +0300

    code edit

--- Ogrenciler/Hatice/Ders23/Movie-Project/src/components/Navbar.jsx
+++ Ogrenciler/Hatice/Ders23/Movie-Project/src/components/Navbar.jsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
-import {
-  Navbar,
-  Container,
-  Nav,
-} from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 const NavBar = () => {
   return (
-    <Navbar expand="lg" data-bs-theme="light" className="mb-4 navbar-section sticky-top">
+    <Navbar
+      expand="lg"
+      data-bs-theme="light"
+      className="mb-4 navbar-section sticky-top"
+    >
       <Container>
         <Navbar.Brand href="#home" className="text-white fw-bold fs-3">
           HdFilmCehennemi
@@ -15,9 +14,15 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link className="text-white fw-bold" href="#home">Home</Nav.Link>
-            <Nav.Link className="text-white fw-bold" href="#filmler">Filmler</Nav.Link>
-            <Nav.Link className="text-white fw-bold" href="#oyuncular">Oyuncular</Nav.Link>
+            <Nav.Link className="text-white fw-bold" href="#home">
+              Home
+            </Nav.Link>
+            <Nav.Link className="text-white fw-bold" href="#filmler">
+              Filmler
+            </Nav.Link>
+            <Nav.Link className="text-white fw-bold" href="#oyuncular">
+              Oyuncular
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
