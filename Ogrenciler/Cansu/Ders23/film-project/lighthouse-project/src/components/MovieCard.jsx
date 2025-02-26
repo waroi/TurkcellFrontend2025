af11ba9cfa5bf0b69234d75859945ed85ca0db65
Emirhan Körhan <korhanemirhann@gmail.com>
2025-02-26 16:51:05 +0300

    responsive added

--- Ogrenciler/Cansu/Ders23/film-project/lighthouse-project/src/components/MovieCard.jsx
+++ Ogrenciler/Cansu/Ders23/film-project/lighthouse-project/src/components/MovieCard.jsx
@@ -46,16 +46,17 @@ const MovieCard = ({movie, genre}) => {
   };
 
   return (
-    <div  className="col-md-5">
+    <div  className="col-md-6">
                     <div className="card mb-3 p-0 card-bg text-light">
                       <div className="row g-0">
-                        <div className="col-md-4">
-                          <CardImg
-                            src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
-                            className="img-fluid rounded-start"
-                            alt="film movie banner"
-                          />
-                        </div>
+                      <div className="col-md-4 text-center text-md-start">
+  <CardImg
+    src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+    className="img-fluid rounded-start"
+    alt="film movie banner"
+  />
+</div>
+
                         <div className="col-md-8">
                           <div className="card-body d-flex flex-column justify-content-between h-100">
                             <CardTitle className="card-title">
@@ -65,7 +66,7 @@ const MovieCard = ({movie, genre}) => {
                               {movie.overview.split(" ").slice(0, 40).join(" ")}
                               ...
                             </CardTypography>
-                            <CardTypography className="d-flex align-items-center gap-1">
+                            <CardTypography className="d-flex align-items-center gap-1 py-3">
                               <i
                                 className="fa-solid fa-star"
                                 style={{ color: "#FFD43B" }}
@@ -73,13 +74,13 @@ const MovieCard = ({movie, genre}) => {
                               <span>{movie.vote_average}</span>
                               <span>({movie.vote_count})</span>
                             </CardTypography>
-                            <CardTypography className="d-flex align-items-center gap-2">
+                            <CardTypography className="d-flex align-items-center gap-2 pb-3">
                               <i className="fa-solid fa-calendar-days"></i>
                               <span> {formatDate(movie.release_date)}</span>
                             </CardTypography>
                             <DetailButton
                               type="button"
-                              className="btn btn-primary w-50"
+                              className="btn btn-primary w-100"
                               data-bs-toggle="modal" data-bs-target={`#userModal-${movie.id}`}
                             >
                               Detayları İncele
--- Ogrenciler/Cansu/Ders23/film-project/lighthouse-project/src/components/Navbar.jsx
+++ Ogrenciler/Cansu/Ders23/film-project/lighthouse-project/src/components/Navbar.jsx
@@ -1,17 +1,17 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const NavbarContainer = styled.nav`
   position: fixed;
   height: 70px;
   top: 0;
   width: 100%;
-  background-color: #1c1c1c;  
+  background-color: #1c1c1c;
 `;
 
 const NavbarBrand = styled.a`
   font-size: 1.5rem;
   font-weight: bold;
-  color: #ffffff;  
+  color: #ffffff;
   text-decoration: none;
 `;
 
@@ -40,55 +40,116 @@ const SearchButton = styled.button`
     background-color: #c9302c;
   }
 `;
-const Navbar = ({ setMoviename, handleSearch, setSelectedOption, selectedOption }) => {
+const Navbar = ({
+  setMoviename,
+  handleSearch,
+  setSelectedOption,
+  selectedOption,
+}) => {
   const functionSearch = (e) => {
-    e.preventDefault()
-    handleSearch()
-    console.log(selectedOption)
-  }
+    e.preventDefault();
+    handleSearch();
+    console.log(selectedOption);
+  };
   return (
-    <NavbarContainer className="navbar navbar-expand-lg navbar-light z-3">
-      <div className="container">
-        <NavbarBrand href="#">CineScope</NavbarBrand>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary positi">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          CineScope
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-
-          <div className="dropdown">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+          <div className="dropdown me-2">
             <button
               className="btn btn-secondary dropdown-toggle"
               type="button"
               data-bs-toggle="dropdown"
               aria-expanded="false"
             >
-              {selectedOption} 
+              {selectedOption}
             </button>
             <ul className="dropdown-menu">
               <li>
-                <button className="dropdown-item" onClick={() => setSelectedOption("Filmler")}>
+                <button
+                  className="dropdown-item"
+                  onClick={() => setSelectedOption("Filmler")}
+                >
                   Filmler
                 </button>
               </li>
               <li>
-                <button className="dropdown-item" onClick={() => setSelectedOption("Oyuncular")}>
+                <button
+                  className="dropdown-item"
+                  onClick={() => setSelectedOption("Oyuncular")}
+                >
                   Oyuncular
                 </button>
               </li>
             </ul>
           </div>
-          <SearchForm>
-            <SearchInput onChange={(e) => setMoviename(e.target.value)} type="search" placeholder="Film ismi..." aria-label="Search" />
-            <SearchButton onClick={functionSearch} type="submit">Ara</SearchButton>
-          </SearchForm>
+          <form className="d-flex" role="search">
+            <input
+              onChange={(e) => setMoviename(e.target.value)}
+              className="form-control me-2"
+              type="search"
+              placeholder="Film ya da oyuncu yaz..."
+              aria-label="Search"
+            />
+            <button onClick={functionSearch} className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
         </div>
       </div>
-    </NavbarContainer>
+    </nav>
+    //     <NavbarContainer className="navbar navbar-expand-lg navbar-light z-3">
+    //       <div className="container gap-5">
+    //         <NavbarBrand href="#" className="d-flex gap-4">CineScope</NavbarBrand>
+    //         <button
+    //   className="navbar-toggler"
+    //   type="button"
+    //   data-bs-toggle="collapse"
+    //   data-bs-target="#navbarNav" // Burada doğru ID kullanıldı
+    //   aria-controls="navbarNav"
+    //   aria-expanded="false"
+    //   aria-label="Toggle navigation"
+    // >
+    //   <span className="navbar-toggler-icon"></span>
+    // </button>
+
+    //         <div
+    //           className="collapse navbar-collapse d-flex align-items-center"
+    //           id="navbarNav"
+    //         >
+    //
+
+    //           <SearchForm className="d-flex flex-grow-1">
+    //             <SearchInput
+    //               className="form-control me-2"
+    //               onChange={(e) => setMoviename(e.target.value)}
+    //               type="search"
+    //               placeholder="Film ismi..."
+    //               aria-label="Search"
+    //             />
+    //             <SearchButton onClick={functionSearch} type="submit">
+    //               Ara
+    //             </SearchButton>
+    //           </SearchForm>
+    //         </div>
+    //       </div>
+    //     </NavbarContainer>
   );
 };
 
 export default Navbar;
-
-
-
-
