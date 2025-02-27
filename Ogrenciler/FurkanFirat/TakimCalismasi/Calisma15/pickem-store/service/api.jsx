9218e17bf6470e208d84891590d0f13a29ca9db5
furkan-firat <furkanfirat@live.com>
2025-02-28 00:11:59 +0300

    update: add useParams

--- Ogrenciler/FurkanFirat/TakimCalismasi/Calisma15/pickem-store/service/api.jsx
+++ Ogrenciler/FurkanFirat/TakimCalismasi/Calisma15/pickem-store/service/api.jsx
@@ -1,6 +1,5 @@
 export const fetchPlayers = async () => {
   const response = await fetch('http://localhost:3000/players');
   const data = await response.json();
-  console.log(data);
   return data;
 };
