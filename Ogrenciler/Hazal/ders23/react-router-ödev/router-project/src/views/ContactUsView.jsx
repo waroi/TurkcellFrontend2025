41894a24f96f2b0ffaba724edaf30552ed52d6ab
YunusOrak <yunusorak08@hotmail.com>
2025-02-27 21:33:30 +0300

    feat: converted to views

---
+++ Ogrenciler/Hazal/ders23/react-router-ödev/router-project/src/views/ContactUsView.jsx
@@ -0,0 +1,15 @@
+import Contact from "../components/Contact/Contact"
+import Footer from "../components/Footer/Footer"
+import Header from "../components/Header/Header"
+
+const ContactUsView = () => {
+    return (
+        <>
+            <Header />
+            <Contact />
+            <Footer />
+        </>
+    )
+}
+
+export default ContactUsView
---
+++ Ogrenciler/Hazal/ders23/react-router-ödev/router-project/src/views/FaqView.jsx
@@ -0,0 +1,15 @@
+import Faq from "../components/Faq/Faq"
+import Footer from "../components/Footer/Footer"
+import Header from "../components/Header/Header"
+
+const FaqView = () => {
+    return (
+        <>
+            <Header />
+            <Faq />
+            <Footer />
+        </>
+    )
+}
+
+export default FaqView
