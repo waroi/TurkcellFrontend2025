50d0f0cae9add94a3a12f93f9db92ce0a2fbd1d7
Mustafa Fatih Gündüz <mustafatihgunduz@gmail.com>
2025-02-27 16:00:30 +0300

    class:ders23-added

---
+++ Ogrenciler/MustafaFatihGunduz/Dersler/Ders23-ReactRouter/router/src/App.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router";
+import Router from "./routes/Router";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <ul>
+        <li>
+          <NavLink to="/">Anasayfa</NavLink>
+        </li>
+        <li>
+          <NavLink to="/user">Kullanıcı</NavLink>
+        </li>
+      </ul>
+      <Router />
+    </>
+  );
+}
+
+export default App;
