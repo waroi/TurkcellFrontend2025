19402c5e435fffa1f6adef2adff5cd178ea03680
ahmetsalihcozel <ahmetsalihcozel@gmail.com>
2025-02-28 15:10:28 +0300

    router

---
+++ Ogrenciler/AhmetSalihCozel/Dersler/Applications/react-router/src/data/contact.js
@@ -0,0 +1,20 @@
+export const contactInfoData = [
+    {
+        id: 1,
+        icon: "bi bi-telephone",
+        title: "Call Us",
+        content: 'Tell: <a href="+12163251729">(216) 325 1729</a>'
+    },
+    {
+        id: 2,
+        icon: "bi bi-envelope-at-fill",
+        title: "E-mail Us",
+        content: 'E-mail: <a href="mailto:info@holoworld.com">info@holoworld.com</a>'
+    },
+    {
+        id: 3,
+        icon: "bi bi-geo-alt-fill",
+        title: "Address",
+        content: 'Cemal Paşa Mahallesi<br> 124.Sokak Dünya Plaza Kat:5<br>Maltepe, İstanbul'
+    }
+];
