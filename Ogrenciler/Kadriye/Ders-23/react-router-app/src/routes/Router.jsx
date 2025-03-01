611489659999cc1e167e8f2776c66c3a961fe636
Sularada <111848753+Sularada@users.noreply.github.com>
2025-03-01 15:49:57 +0300

    Ders-24

--- Ogrenciler/Kadriye/Ders-23/react-router-app/src/routes/Router.jsx
+++ Ogrenciler/Kadriye/Ders-23/react-router-app/src/routes/Router.jsx
@@ -1,13 +1,19 @@
-import { useRoutes } from "react-router";
+import { Navigate, useRoutes } from "react-router";
 import HomeView from "../views/HomeView";
 import UserView from "../views/UserView";
 import userRouter from "./UserRouter";
 import ParameterView from "../views/ParameterView";
+import NewView from "../views/NewView";
 const Router = () => {
   const routes = useRoutes([
     { path: "/", element: <HomeView /> },
     userRouter,
     { path: "/parameter/:id", element: <ParameterView /> },
+    {
+      path: "/haberler/",
+      element: <NewView />,
+      children: [{ index: true, element: <Navigate to="Spor" /> }],
+    },
     // { path: "/user", element: <UserView /> }
   ]);
   return routes;
