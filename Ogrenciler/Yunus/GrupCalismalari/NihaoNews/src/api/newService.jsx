3543a244491e2ebae2974fe119061ee194c3e662
yunuso-me <yunusorak08@hotmail.com>
2025-03-01 21:09:58 +0300

    update news

--- Ogrenciler/Yunus/GrupCalismalari/NihaoNews/src/api/newService.jsx
+++ Ogrenciler/Yunus/GrupCalismalari/NihaoNews/src/api/newService.jsx
@@ -1,11 +1,10 @@
 const baseUrl = import.meta.env.VITE_API_URL;
 const apiKey = import.meta.env.VITE_API_KEY;
 
-export class newsService{
-    static async getNews(page){
-        const response = await fetch(baseUrl + `?q=sport&page=${page}&apiKey=${apiKey}&language=jp`)
+export class newsService {
+    static async getNews(page) {
+        const response = await fetch(baseUrl + `?q=sport&page=${page}&apiKey=${apiKey}&language=jp&pageSize=10`)
         const data = await response.json()
-        return data.articles           
+        return data
     }
-
 }
