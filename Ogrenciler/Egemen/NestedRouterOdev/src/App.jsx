bae6e0a06081eda141ec0a446a9e90b434aa02ed
greenmagican <egemenaksoz98@gmail.com>
2025-03-01 16:15:26 +0300

    update
    update

--- Ogrenciler/Egemen/NestedRouterOdev/src/App.jsx
+++ Ogrenciler/Egemen/NestedRouterOdev/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [articles, setArticles] = useState([]);
+  const [categories, setCategories] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    getArticles();
+  });
+
+  const getArticles = async () => {
+    const url =
+      "https://newsapi.org/v2/everything?" +
+       q=`{}` +
+      "language=tr&" +
+      "from=2025-02-27&" +
+      "sortBy=popularity&" +
+      "apiKey=b9b9267a4400423eb272474b19898915";
+
+    fetch(url)
+      .then((response) => response.json()) // JSON olarak ayrıştır
+      .then((data) => setArticles(articles)) // Ayrıştırılmış veriyi yazdır
+  
+      .catch((error) => console.error("Hata:", error)); // Hata yakalama
+  };
+
+  return <div>Check the console for news articles.</div>;
 }
 
-export default App
+export default App;
