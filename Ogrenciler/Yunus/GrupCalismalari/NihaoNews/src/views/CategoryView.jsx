2d8080c56f8d84769ea7da378827f61bb0a28904
Cansu126 <cevikcnsu126@gmail.com>
2025-03-02 21:44:55 +0300

    update

--- Ogrenciler/Yunus/GrupCalismalari/NihaoNews/src/views/CategoryView.jsx
+++ Ogrenciler/Yunus/GrupCalismalari/NihaoNews/src/views/CategoryView.jsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from 'react';
-import { NavLink, useParams } from 'react-router';
+import { useParams } from 'react-router';
 import { newsService } from '../api/newService';
+import Card from '../components/Card';
 
 const CategoryView = () => {
   const [news, setNews] = useState();
   const { categoryName } = useParams();
-  const defaultImgUrl =
-    "https://img.freepik.com/free-psd/3d-rendering-news-sales-background_23-2150732563.jpg?t=st=1740909615~exp=1740913215~hmac=e24e8489fcda35d4d46b0bab5e7e7c30b3b2975224599f03f94e00f11eddf16c&w=2000";
-
+  
   useEffect(() => {
     const getnews = async () => {
       const response = await newsService.getNews(categoryName.toString());
@@ -26,33 +25,14 @@ const CategoryView = () => {
                   key={index}
                   className="col-lg-3 new-item my-4 rounded col-md-4 col-sm-6 col-12"
                 >
-                  <NavLink
-                    to={`/news/${item.id}`}
-                    className="h-100 d-flex flex-column"
-                  >
-                    <div className="new-image">
-                      <img
-                        src={item.urlToImage}
-                        onError={({ currentTarget }) => {
-                          currentTarget.onerror = null;
-                          currentTarget.src = defaultImgUrl;
-                        }}
-                        className="img-fluid h-100 w-100 object-fit-cover"
-                        alt={item.title}
-                      />
-                    </div>
-                    <h4 className="my-3">{item.title}</h4>
-                    <p className="new-description">
-                      {item.description.split("").slice(0, 40).join("")}
-                    </p>
-                  </NavLink>
+                  <Card item={item}/>
                 </div>
               );
             })}
           </div>
         </div>
       ) : (
-        <h1>Bulunamadı</h1>
+        <h1>Yükleniyor...</h1>
       )}
     </>
   )
