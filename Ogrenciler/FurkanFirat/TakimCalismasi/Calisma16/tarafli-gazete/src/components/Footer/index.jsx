cdb76fe2e69be3f6168f9d93c3eedfd67aa04c37
Berfin Ela Kılıç <77438033+Elakilic@users.noreply.github.com>
2025-03-03 16:43:44 +0300

    update: card updated. footer added.

---
+++ Ogrenciler/FurkanFirat/TakimCalismasi/Calisma16/tarafli-gazete/src/components/Footer/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div class="footer py-5 text-center bg-primary text-white">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <p class="mb-0">
+              Copyright ©2025 All rights reserved | This website is made with
+              <img
+                src="https://img.icons8.com/?size=20&id=59805&format=png&color=d70000"
+              />
+              by Ela, Furkan and Ahmet.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
