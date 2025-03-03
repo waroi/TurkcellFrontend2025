1e9a9e1934dff7a16146843df4082eb215a943bd
Berfin Ela Kılıç <77438033+Elakilic@users.noreply.github.com>
2025-03-03 16:51:41 +0300

    .env

--- Ogrenciler/FurkanFirat/TakimCalismasi/Calisma16/tarafli-gazete/src/components/Footer/index.jsx
+++ Ogrenciler/FurkanFirat/TakimCalismasi/Calisma16/tarafli-gazete/src/components/Footer/index.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div class="footer py-5 text-center bg-primary text-white">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <p class="mb-0">
+    <div className="footer py-5 text-center bg-primary text-white">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <p className="mb-0">
               Copyright ©2025 All rights reserved | This website is made with
               <img
                 src="https://img.icons8.com/?size=20&id=59805&format=png&color=d70000"
--- Ogrenciler/FurkanFirat/TakimCalismasi/Calisma16/tarafli-gazete/src/service/api.jsx
+++ Ogrenciler/FurkanFirat/TakimCalismasi/Calisma16/tarafli-gazete/src/service/api.jsx
@@ -1,5 +1,6 @@
+
 const BASE_URL = 'https://newsapi.org/v2/top-headlines';
-const API_KEY = '71a9b81b167343738dfd6bb4bb2fa41d';
+
 
 export const fetchNews = async (category, query = '') => {
   const response = await fetch(
@@ -7,7 +8,7 @@ export const fetchNews = async (category, query = '') => {
     {
       method: 'GET',
       headers: {
-        Authorization: API_KEY,
+        Authorization: import.meta.env.VITE_API_KEY,
       },
     }
   );
