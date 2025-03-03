d86c51b7ce5805ce0d540c120d0264c9ad53f94f
elinoza <hillcakmak@gmail.com>
2025-03-03 04:15:14 +0300

    chilren routes added

--- Ogrenciler/Hilal/takimCalismasi/news/components/Navbar.jsx
+++ Ogrenciler/Hilal/takimCalismasi/news/components/Navbar.jsx
@@ -1,31 +1,32 @@
 import React from "react";
 import { useNavigate } from "react-router";
 
-const Navbar = ({country, topic, setCountry,setTopic}) => {
+const Navbar = () => {
   const navigate = useNavigate();
+
   const handleCountryChange = (event) => {
     const selectedCountry = event.target.value;
-    setCountry(selectedCountry)
+    // setCountry(selectedCountry);
     if (selectedCountry) {
       navigate(`/${selectedCountry}`);
     }
   };
 
-  const handleTopicChange = (event) => {
-    const selectedTopic = event.target.value;
-    setTopic(selectedTopic)
-    if (selectedTopic) {
-      navigate(`/${country}/${selectedTopic}`);
-    }
-  };
-  
+  // const handleTopicChange = (event) => {
+  //   const selectedTopic = event.target.value;
+  //   setTopic(selectedTopic);
+  //   if (selectedTopic) {
+  //     navigate(`/${country}/${selectedTopic}`);
+  //   }
+  // };
+
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container">
         <a className="navbar-brand" href="#">
           Navbar
         </a>
-        <button
+        {/* <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
@@ -35,8 +36,8 @@ const Navbar = ({country, topic, setCountry,setTopic}) => {
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        </button> */}
+        {/* <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#">
@@ -64,19 +65,31 @@ const Navbar = ({country, topic, setCountry,setTopic}) => {
               </a>
             </li>
           </ul>
-        </div>
-      <select className="form-select" aria-label="Default select example" onChange={handleCountryChange}>
-        <option selected defaultValue="tr">Türkiye</option>
-        <option value="de">Almanya</option>
-        <option value="ru">Rusya</option>
-        <option value="en">İngiltere</option>
-      </select>
-      <select className="form-select" aria-label="Default select example" onChange={handleTopicChange}>
-        <option selected defaultValue="general">Genel</option>
-        <option value="sport">Spor</option>
-        <option value="technology">Teknoloji</option>
-        <option value="economy">Ekonomi</option>
-      </select>
+        </div> */}
+        <select
+          className="form-select"
+          aria-label="Default select example"
+          onChange={handleCountryChange}
+        >
+          <option selected defaultValue="tr">
+            Türkiye
+          </option>
+          <option value="de">Almanya</option>
+          <option value="ru">Rusya</option>
+          <option value="en">İngiltere</option>
+        </select>
+        {/* <select
+          className="form-select"
+          aria-label="Default select example"
+          onChange={handleTopicChange}
+        >
+          <option selected defaultValue="general">
+            Genel
+          </option>
+          <option value="sport">Spor</option>
+          <option value="technology">Teknoloji</option>
+          <option value="economy">Ekonomi</option>
+        </select> */}
       </div>
     </nav>
   );
--- Ogrenciler/Hilal/takimCalismasi/news/views/HomeView.jsx
+++ Ogrenciler/Hilal/takimCalismasi/news/views/HomeView.jsx
@@ -1,83 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { Outlet, NavLink } from "react-router";
 
-const HomeView = ({news}) => {
-
+const HomeView = () => {
   return (
     <div>
       <h2>Haberler</h2>
-      <div id="carouselExampleIndicators" className="carousel slide">
-        <div className="carousel-indicators">
-          {news.map((_, index) => (
-            <button
-              key={index}
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to={index}
-              className={index === 0 ? "active" : ""}
-              aria-current={index === 0 ? "true" : undefined}
-              aria-label={`Slide ${index + 1}`}
-            ></button>
-          ))}
-        </div>
-
-        <div className="carousel-inner">
-          {news.map((item, index) => (
-            <div
-              className={`${
-                item.key == 0 ? "carousel-item active" : "carousel-item"
-              }`}
-              key={item.key}
-            >
-              <div className="card mb-3">
-                <div className="row g-0">
-                  <div className="col-md-4">
-                    <img
-                      src={item?.image}
-                      className="img-fluid rounded-start"
-                      alt="..."
-                    />
-                  </div>
-                  <div className="col-md-8">
-                    <div className="card-body">
-                      <h5 className="card-title">{item?.name}</h5>
-                      <p className="card-text">{item?.description}</p>
-                      <p className="card-text">
-                        <small className="text-body-secondary">
-                          Kaynak: {item?.source}
-                        </small>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
+      <Outlet />
     </div>
   );
 };
