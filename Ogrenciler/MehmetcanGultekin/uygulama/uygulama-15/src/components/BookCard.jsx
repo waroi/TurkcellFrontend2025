d9509294c4dedf1a7322785fa8d4a61858860b69
eceiremklc <eceiremklc@gmail.com>
2025-03-04 16:52:05 +0300

    some style fixes.

--- Ogrenciler/MehmetcanGultekin/uygulama/uygulama-15/src/components/BookCard.jsx
+++ Ogrenciler/MehmetcanGultekin/uygulama/uygulama-15/src/components/BookCard.jsx
@@ -1,46 +1,41 @@
 import React, { useState } from "react";
 import "../components/BookCard.css";
+import "../App.css";
 const BookCard = () => {
-    const [isFlipped, setFlipped] = useState(false);
+  const [isFlipped, setFlipped] = useState(false);
 
-    const handleFlip = () => {
-        setFlipped(!isFlipped);
-    };
+  const handleFlip = () => {
+    setFlipped(!isFlipped);
+  };
 
-    return (
-        <div className="App">
-            <div className="container">
-                <div className="row">
-                    <div className="col col-lg-4">
-                        <div
-                            className={`flip-card ${isFlipped ? "flipped" : ""
-                                }`}
-                        >
-                            <div onClick={handleFlip} className="flip-card-inner">
-
-                                <div className="flip-card-front">
-                                    <div className="card-content">
-                                        <img className="w-100 h-100 img-fluid object-fit-cover" src="https://i.idefix.com/resize/800/0/cache/600x600-0/originals/0000000105599-1.jpg" alt="felsefe" />
-                                    </div>
-                                </div>
-                                <div className="flip-card-back">
-                                    <div className="card-content">
-                                        <p>Harry Potter ve Felsefe Taşı</p>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-
-
-                    </div>
-
+  return (
+    <div className="bookcard">
+      <div className="container py-4">
+        <div className="row">
+          <div className="col col-lg-4">
+            <div className={`flip-card ${isFlipped ? "flipped" : ""}`}>
+              <div onClick={handleFlip} className="flip-card-inner">
+                <div className="flip-card-front">
+                  <div className="card-content">
+                    <img
+                      className="w-100 h-100 img-fluid object-fit-cover"
+                      src="https://i.idefix.com/resize/800/0/cache/600x600-0/originals/0000000105599-1.jpg"
+                      alt="felsefe"
+                    />
+                  </div>
                 </div>
-
+                <div className="flip-card-back">
+                  <div className="card-content p-3">
+                    <p>Harry Potter ve Felsefe Taşı</p>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default BookCard
+export default BookCard;
--- Ogrenciler/MehmetcanGultekin/uygulama/uygulama-15/src/components/Footer.jsx
+++ Ogrenciler/MehmetcanGultekin/uygulama/uygulama-15/src/components/Footer.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 const Footer = () => {
-    return (
-        <div className="footer-bottom fixed-bottom py-2">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <p className="mb-0 pb-3 d-flex justify-content-center">
-                            Copyright 2025 All rights reserved | This website is made with
-                            <img src="https://img.icons8.com/?size=30&id=59805&format=png&color=ff0000" />
-                            by Mehmetcan, Ece and Hilal.
-                        </p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="footer-bottom py-2">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <p className="mb-0 pb-3 d-flex justify-content-center">
+              Copyright 2025 All rights reserved | This website is made with
+              <img src="https://img.icons8.com/?size=30&id=59805&format=png&color=ff0000" />
+              by Mehmetcan, Ece and Hilal.
+            </p>
+          </div>
         </div>
-
-);
+      </div>
+    </div>
+  );
 };
 export default Footer;
