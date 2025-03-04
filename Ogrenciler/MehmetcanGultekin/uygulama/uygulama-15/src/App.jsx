95ba4ef1d77cadd38b2dad80ad09a60920acb72e
elinoza <hillcakmak@gmail.com>
2025-03-05 00:25:19 +0300

    redux built

--- Ogrenciler/MehmetcanGultekin/uygulama/uygulama-15/src/App.jsx
+++ Ogrenciler/MehmetcanGultekin/uygulama/uygulama-15/src/App.jsx
@@ -1,4 +1,13 @@
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addBook,
+  deleteBook,
+  updateBook,
+  filterBooks,
+  sortBooksByDate,
+  sortBooksByString,
+} from "./redux/slices/bookSlice";
 import "./App.css";
 import BookCard from "./components/BookCard";
 import Navbar from "./components/Navbar";
@@ -12,18 +21,57 @@ import { books as booksData } from "../books.json";
  */
 
 function App() {
-  const [books, setBooks] = useState([]);
+  const booksFromStore = useSelector((state) => state.book.books);
+  const dispatch = useDispatch();
+  const handleDelete = (id) => dispatch(deleteBook(id));
+  const handleAddBook = (book) => dispatch(addBook(book));
+  const handleFilterBook = (category) => dispatch(filterBooks(category));
+  const handleUpdateBook = (id, book) => {
+    (book = { id, ...book }), dispatch(updateBook(book));
+  };
+
+  console.log("from store", booksFromStore);
   console.log("booksData", booksData);
+
   useEffect(() => {
-    setBooks(booksData);
-    const booksFromStorage = JSON.parse(localStorage.getItem("books"));
+    if ("books" in localStorage) {
+      return;
+    } else {
+      console.log("hello");
+      localStorage.setItem("books", JSON.stringify(booksData));
+    }
   }, []);
-  useEffect(() => {
-    localStorage.setItem("books", JSON.stringify(books));
-  }, [books]);
 
   return (
     <>
+      <button onClick={() => handleFilterBook("fantastic")}>
+        Fantastik olanları getir
+      </button>
+      <button
+        onClick={() =>
+          handleAddBook({
+            id: "1",
+            author: "fhfgh",
+            title: "Hfghfhfh",
+            description: "fghfh",
+            img_url: "https://example.com/hp1.jpg",
+            created_at: "1997-06-26T00:00:00Z",
+            category: "Fantastik",
+          })
+        }
+      >
+        ekle
+      </button>
+      <button
+        onClick={() =>
+          handleUpdateBook("1a2b3c4d", {
+            author: "Ece",
+          })
+        }
+      >
+        Değiştir
+      </button>
+      <button onClick={() => handleDelete("1")}>sil</button>
       <Navbar />
       <BookCard />
       <Footer />
