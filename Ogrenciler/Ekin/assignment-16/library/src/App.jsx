5fc520a05a1291f5a132592d6b46bde0cb6fc0e6
Ekin Aslan <ekinaslan.js@gmail.com>
2025-03-05 00:08:44 +0300

    Library State CRUD Operations

--- Ogrenciler/Ekin/assignment-16/library/src/App.jsx
+++ Ogrenciler/Ekin/assignment-16/library/src/App.jsx
@@ -2,37 +2,67 @@ import { useState } from "react";
 import Navigation from "./components/Navigation";
 import Card from "./components/Card";
 import Modal from "./components/Modal";
-import { Container } from "react-bootstrap";
-import { useSelector, useDispatch } from "react-redux";
+import { Container, Button } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { addBook } from "./redux/slices/librarySlice";
 
 export default function App() {
-  const books = useSelector((state) => state.library.books);
   const dispatch = useDispatch();
-  // const handleDelete = (id) => dispatch(deleteTodo(id));
   const handleAddBook = (book) => dispatch(addBook(book));
 
-  console.log(books);
+  const [show, setShow] = useState(false);
+  const [modalProperties, setModalProperties] = useState({
+    mode: null,
+    book: null,
+    action: null,
+  });
+
+  const books = useSelector((state) => state.library.books);
 
   return (
     <>
-      <button onClick={() => handleAddBook({ id: "15", name: "TEST" })}>
-        HELLO
-      </button>
-      {books.map((t, i) => (
-        <p key={i}>{t.name}</p>
-      ))}
       <Navigation />
+      <Container className="d-flex justify-content-center mb-3">
+        <Button
+          onClick={() => {
+            setModalProperties({
+              mode: "add",
+              action: handleAddBook,
+            });
+
+            setShow(true);
+          }}
+        >
+          <i className="fa-solid fa-plus"></i> Add Book
+        </Button>
+      </Container>
       <main>
         <Container>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {books?.map((book) => (
+            <Card
+              key={book.id}
+              book={book}
+              setShow={setShow}
+              setModalProperties={setModalProperties}
+            />
+          ))}
         </Container>
       </main>
-      <Modal />
+      <Modal
+        show={show}
+        setShow={setShow}
+        modalProperties={modalProperties}
+        // mode="edit"
+        // book={{
+        //   id: 8,
+        //   name: "Computer Organization and Design",
+        //   desc: "Computer architecture, processors, and assembly language.",
+        //   author: "David A. Patterson, John L. Hennessy",
+        //   image:
+        //     "https://tse2.mm.bing.net/th?id=OIP.M-5VcD99nqGQNXhzaVc-YAHaJH&pid=Api",
+        // }}
+      />
     </>
   );
 }
