e39c09a3f978d102ef0693e10cd29bfe6dfdce56
yunuso-me <yunusorak08@hotmail.com>
2025-03-06 21:56:08 +0300

    feat: Added Firebase authentication (login & signup)

---
+++ Ogrenciler/Yunus/GrupCalismalari/Library/src/views/RegisterView.jsx
@@ -0,0 +1,21 @@
+import { onAuthStateChanged } from "firebase/auth";
+import Register from "../components/Login/Register"
+import { auth } from "../firebase/firebase";
+import { Navigate, useNavigate } from "react-router";
+import { useEffect } from "react";
+
+const RegisterView = () => {
+    const navigate = useNavigate()
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                return navigate('/')
+            }
+        });
+    }, [])
+    return (
+        <Register login={true} />
+    )
+}
+
+export default RegisterView
