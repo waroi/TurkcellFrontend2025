c15039d0b1938def1cd5adc5cfd8874c93e223cb
Fatih Taner <ibrahimfatihtaner@gmail.com>
2025-03-06 21:01:58 +0300

    feature: started to firebase implementation

---
+++ Ogrenciler/FurkanFirat/TakimCalismasi/Calisma17/book-store/src/firebase/auth.jsx
@@ -0,0 +1,28 @@
+import { auth } from "./firebase.config";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
