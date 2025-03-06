e5c24ab427a2fa06faa59c6598934ac82bf52490
Cansu126 <cevikcnsu126@gmail.com>
2025-03-07 01:29:09 +0300

    added firebase

--- Ogrenciler/Aybike/Applications/book-collection-project/src/controller/AuthController.js
+++ Ogrenciler/Aybike/Applications/book-collection-project/src/controller/AuthController.js
@@ -1,4 +1,4 @@
-import { auth } from "../../firebase";
+import { auth } from "../firebase/firebase";
 import { readUser, saveUser } from "./DBController";
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 
