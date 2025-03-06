a74ce53f81f0f9ebb72a6d45db06485cda8253c3
YunusOrak <yunusorak08@hotmail.com>
2025-03-06 12:30:20 +0300

    update

--- Ogrenciler/Yunus/GrupCalismalari/Library/src/App.jsx
+++ Ogrenciler/Yunus/GrupCalismalari/Library/src/App.jsx
@@ -2,13 +2,13 @@ import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Router from './routes/Router'
 import './App.css'
-import signed from './api/fireService'
 import { useEffect } from 'react'
+import { Auth } from './api/auth'
 function App() {
 
-useEffect(() => {
-  signed()
-}, [])
+  useEffect(() => {
+    Auth.authOnStateChanged()
+  }, [])
 
 
   return (
