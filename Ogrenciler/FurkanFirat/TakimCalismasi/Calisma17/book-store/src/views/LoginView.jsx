7f8bcef083e4abf6030ce7fa9e4633f54987d306
Fatih Taner <ibrahimfatihtaner@gmail.com>
2025-03-06 23:31:05 +0300

    feature: firebase implementation continue

--- Ogrenciler/FurkanFirat/TakimCalismasi/Calisma17/book-store/src/views/LoginView.jsx
+++ Ogrenciler/FurkanFirat/TakimCalismasi/Calisma17/book-store/src/views/LoginView.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import { doSignInWithEmailAndPassword } from '../firebase/auth';
-import { useAuth } from '../context/authContext';
-import { Navigate } from 'react-router';
+import React, { useState } from "react";
+import { doSignInWithEmailAndPassword } from "../firebase/auth";
+import { useAuth } from "../context/authContext";
+import { Navigate } from "react-router";
 
 const LoginView = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [isLogginIng, setIsLoggingIn] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
 
   const { isLoggedIn } = useAuth();
 
@@ -21,23 +21,23 @@ const LoginView = () => {
 
   return (
     <>
-      {isLoggedIn && <Navigate to='/' replace={true} />}
+      {isLoggedIn && <Navigate to="/" replace={true} />}
       <div>
         LOGIN
         <form onSubmit={handleSubmit}>
           <input
-            type='email'
+            type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            placeholder='Email'
+            placeholder="Email"
           />
           <input
-            type='password'
+            type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            placeholder='Password'
+            placeholder="Password"
           />
-          <button type='submit'>Login</button>
+          <button type="submit">Login</button>
         </form>
       </div>
     </>
