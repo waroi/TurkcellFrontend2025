985796aef2761c8fdbe46443462cde57d4b21e50
furkan-firat <furkanfirat@live.com>
2025-03-07 18:02:51 +0300

    feature: add private route

--- Ogrenciler/FurkanFirat/TakimCalismasi/Calisma17/book-store/src/Routes/Router.jsx
+++ Ogrenciler/FurkanFirat/TakimCalismasi/Calisma17/book-store/src/Routes/Router.jsx
@@ -3,13 +3,20 @@ import HomeView from '../views/HomeView';
 import AdminView from '../views/AdminView';
 import LoginView from '../views/LoginView';
 import RegisterView from '../views/RegisterView';
+import { useAuth } from '../context/authContext';
+import PrivateRoute from './PrivateRoute';
 
 export default function Router() {
+  const { isLoggedIn } = useAuth();
   const router = useRoutes([
     { path: '/', element: <HomeView /> },
     { path: '/login', element: <LoginView /> },
     { path: '/register', element: <RegisterView /> },
-    { path: '/admin', element: <AdminView /> },
+    {
+      path: '/admin',
+      element: <PrivateRoute isLoggedIn={isLoggedIn} />,
+      children: [{ index: true, element: <AdminView /> }],
+    },
   ]);
   return router;
 }
