214bc2ae5acc8b44fccf43c701fe1f4be7a0d081
Mustafa Cenk Aydın <402523@ogr.ktu.edu.tr>
2025-03-07 20:48:26 +0300

    fix: todo return

--- Ogrenciler/Hikmet/todos/src/App.jsx
+++ Ogrenciler/Hikmet/todos/src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from "react";
+import {useState} from "react";
 import "./App.css";
-import { useStore } from "./store/index";
+import {useStore} from "./store/index";
 
 function App() {
-	const { todo, addTodo, toggleTodo, deleteTodo } = useStore();
-	const [inputTodo, setInputTodo] = useState("");
+  const {todo, addTodo, toggleTodo, deleteTodo} = useStore();
+  const [inputTodo, setInputTodo] = useState("");
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		addTodo(inputTodo);
-	};
-	return (
-		<>
-			<section>
-				<h1>Yapılmışlar Listesi</h1>
-				<form onSubmit={handleSubmit}>
-					<input type="text" onChange={(e) => setInputTodo(e.target.value)} />
-					<button>Ekle</button>
-				</form>
-				<div>
-					{todo.map((todo) => {
-						<>
-							<h1>{todo.text}</h1>
-							<p onClick={toggleTodo}>{todo.id}</p>;
-						</>;
-					})}
-				</div>
-			</section>
-		</>
-	);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(inputTodo);
+  };
+  return (
+    <>
+      <section>
+        <h1>Yapılmışlar Listesi</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" onChange={(e) => setInputTodo(e.target.value)} />
+          <button>Ekle</button>
+        </form>
+        <div>
+          {todo.map((todo) => {
+            return (
+              <div key={todo.id}>
+                <h1>{todo.text}</h1>
+                <p onClick={() => toggleTodo(todo.id)}>{todo.id}</p>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default App;
