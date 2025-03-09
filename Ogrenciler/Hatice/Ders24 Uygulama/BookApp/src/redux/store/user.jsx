aa346ef364a1291957f69e876902ccf26368ba60
haticeozcelik <hatice.ozceelik@hotmail.com>
2025-03-09 20:20:37 +0300

    .

---
+++ Ogrenciler/Hatice/Ders24 Uygulama/BookApp/src/redux/store/user.jsx
@@ -0,0 +1,16 @@
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchUserPublisherId } from "../slice/user";
+
+// const MyComponent = () => {
+//   const dispatch = useDispatch();
+//   const publisherId = useSelector((state) => state.user.publisherId);
+
+//   useEffect(() => {
+//     dispatch(fetchUserPublisherId());
+//   }, [dispatch]);
+
+//   return <div>Publisher ID: {publisherId}</div>;
+// };
+
+// export default MyComponent;
