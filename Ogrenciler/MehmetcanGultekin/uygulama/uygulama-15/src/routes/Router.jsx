f86d43edb1fb732d8e287c68089e0f32add487ac
elinoza <hillcakmak@gmail.com>
2025-03-09 16:22:30 +0300

    login-signup addded, props not working, zustand eklenecek

--- Ogrenciler/MehmetcanGultekin/uygulama/uygulama-15/src/routes/Router.jsx
+++ Ogrenciler/MehmetcanGultekin/uygulama/uygulama-15/src/routes/Router.jsx
@@ -1,10 +1,21 @@
-import { useRoutes } from "react-router";
+import { useRoutes, Navigate } from "react-router";
 import HomeView from "../views/HomeView";
 import AuthView from "../views/AuthView";
+import SignUpForm from "../components/SignUpForm";
+import LoginForm from "../components/LoginForm";
 
 const Router = () => {
   const routes = useRoutes([
-    { path: "/", element: <AuthView /> },
+    {
+      path: "/",
+      element: <AuthView />,
+      children: [
+        { index: true, element: <Navigate to="/login" /> },
+        { path: "/login", element: <LoginForm /> },
+        { path: "/signup", element: <SignUpForm /> },
+      ],
+    },
+
     { path: "/app", element: <HomeView /> },
   ]);
   return routes;
--- Ogrenciler/MehmetcanGultekin/uygulama/uygulama-15/src/views/AuthView.jsx
+++ Ogrenciler/MehmetcanGultekin/uygulama/uygulama-15/src/views/AuthView.jsx
@@ -1,33 +1,38 @@
-import { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
 import {
   createUserWithEmailAndPassword,
+  SignInMethod,
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import { auth, db } from "../../firebase-config";
 import { useNavigate } from "react-router";
 import "../App.css";
 import { doc, setDoc } from "firebase/firestore";
+import SignUpForm from "../components/SignUpForm";
+import LoginForm from "../components/LoginForm";
+import { Outlet } from "react-router";
+
 const AuthView = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [adminName, setAdminName] = useState("");
-  const [yayinevi, setYayinevi] = useState("");
   const navigate = useNavigate();
-  const handleSignIn = async () => {
-    const userCredential = await signInWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    setEmail("");
-    setPassword("");
-    setAdminName("");
-    setYayinevi("");
-    navigate("/app");
+
+  const handleSignIn = async (password, email) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log("response", userCredential);
+
+      navigate("/app");
+    } catch (signinError) {
+      if (signinError.code === "auth/user-not-found") {
+        console.error(signinError);
+        navigate("/signUp");
+      }
+    }
   };
-  const handleSignUp = async () => {
+
+  const handleSignUp = async (password, email, adminName, yayinevi) => {
     const userCredential = await createUserWithEmailAndPassword(
       auth,
       email,
@@ -51,70 +56,27 @@ const AuthView = () => {
       }
     }
   };
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+
+  const handleSubmit = async (password, email, adminName, yayinevi) => {
+    console.log(password, email, adminName, yayinevi);
     try {
-      await handleSignUp();
+      await handleSignUp(password, email, adminName, yayinevi);
       console.log("✅ Kayıt başarılı!");
     } catch (signupError) {
       console.error("❌ Kayıt başarısız:", signupError.message);
       if (signupError.code === "auth/email-already-in-use") {
         console.log("📌 Giriş deneniyor:", email);
-        await handleSignIn();
+        await handleSignIn(password, email);
         console.log("✅ Giriş başarılı!");
       }
     }
   };
+
   return (
     <div className=" loginscreen w-100 d-flex align-items-center justify-content-center vh-100">
-      <div className="container flex-column w-25 p-5 bg-brown text-white rounded-5">
-        <h2 className="mb-5 text-center">Giriş Yap / Kayıt Ol</h2>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              type="email"
-              className="rounded-pill py-2"
-              placeholder="Email giriniz."
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Şifre</Form.Label>
-            <Form.Control
-              type="password"
-              className="rounded-pill py-2 mb-3"
-              placeholder="Şifre giriniz."
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formAdminName">
-            <Form.Label>Admin Adı</Form.Label>
-            <Form.Control
-              type="text"
-              className="rounded-pill py-2 mb-3"
-              placeholder="Admin adınızı giriniz."
-              value={adminName}
-              onChange={(e) => setAdminName(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formYayinevi">
-            <Form.Label>Yayınevi</Form.Label>
-            <Form.Control
-              type="text"
-              className="rounded-pill py-2 mb-5"
-              placeholder="Yayınevi giriniz."
-              value={yayinevi}
-              onChange={(e) => setYayinevi(e.target.value)}
-            />
-          </Form.Group>
-          <Button variant="light" className="rounded-pill" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </div>
+      <Outlet />
+      {/* <SignUpForm handleSubmit={handleSubmit} /> */}
+      {/* {<LoginForm handleSignIn={handleSignIn} />} */}
     </div>
   );
 };
