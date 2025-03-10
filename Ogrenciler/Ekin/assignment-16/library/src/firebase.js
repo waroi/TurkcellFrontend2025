25fec000fc311622ac59d1d9875d3aa9090ef187
Ekin Aslan <ekinaslan.js@gmail.com>
2025-03-10 18:07:10 +0300

    Fix

--- Ogrenciler/Ekin/assignment-16/library/src/firebase.js
+++ Ogrenciler/Ekin/assignment-16/library/src/firebase.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "firebase/app";
+
 import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "firebase/auth";
+
 import {
   getFirestore,
   collection,
