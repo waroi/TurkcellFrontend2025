8ff16766a2c805c128fe0679175dcaeb891f5894
Hikmet Melik Fırat <hikmetmelik@gmail.com>
2025-03-10 16:43:00 +0300

    refactor: improve component structure and formatting in BannerView, Home, SubscribeView, and NavbarView

--- Ogrenciler/MustafaFatihGunduz/Library/library-app/src/Pages/Home.jsx
+++ Ogrenciler/MustafaFatihGunduz/Library/library-app/src/Pages/Home.jsx
@@ -1,5 +1,4 @@
 import "../App.scss";
-import { ThemeProvider } from "../context/ThemeContext";
 import BannerView from "../components/BannerView";
 import BooksView from "../components/BooksView";
 import Footer from "../components/FooterView";
@@ -8,34 +7,35 @@ import PopularNow from "../components/PopularNow";
 import Recommended from "../components/Recommended";
 import SpecialOfferView from "../components/SpecialOfferView";
 import SubscribeView from "../components/SubscribeView";
+import { ThemeProvider } from "../context/ThemeContext";
 import RightSide from "../Pages/RightSide";
 import Sidebar from "../Pages/Sidebar";
 
 const Home = () => {
-  return (
-    <>
-      <ThemeProvider>
-      <NavbarView />
-      <div className="wrapper">
-        <div className="side-left">
-          <Sidebar />
-        </div>
-        <div className="side-middle d-flex flex-column align-items-center justify-content-center">
-          <BannerView />
-		      <Recommended />
-          <PopularNow />
-          <BooksView />
-          <SpecialOfferView />
-          <SubscribeView />
-          <Footer />
-        </div>
-		<div className="side-right">
-			<RightSide />
-		</div>
-      </div>
-      </ThemeProvider>
-    </>
-  );
+	return (
+		<>
+			<ThemeProvider>
+				<NavbarView />
+				<div className="wrapper">
+					<div className="side-left">
+						<Sidebar />
+					</div>
+					<div className="side-middle d-flex flex-column align-items-center justify-content-center">
+						<BannerView />
+						<Recommended />
+						<PopularNow />
+						<BooksView />
+						<SpecialOfferView />
+						<SubscribeView />
+						<Footer />
+					</div>
+					<div className="side-right">
+						<RightSide />
+					</div>
+				</div>
+			</ThemeProvider>
+		</>
+	);
 };
 
 export default Home;
