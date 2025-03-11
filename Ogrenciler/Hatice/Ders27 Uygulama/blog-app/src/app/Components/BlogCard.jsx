5a899eb81a30f4091173905721995677d021ef06
elinoza <hillcakmak@gmail.com>
2025-03-11 23:08:38 +0300

    working fine

--- Ogrenciler/Hatice/Ders27 Uygulama/blog-app/src/app/Components/BlogCard.jsx
+++ Ogrenciler/Hatice/Ders27 Uygulama/blog-app/src/app/Components/BlogCard.jsx
@@ -1,25 +1,38 @@
+"use client";
 import Link from "next/link";
+import Image from "next/image";
 
 const BlogCard = ({ blog }) => {
-    return (
-        <>
-            {blog && <div className="col-lg-3 rounded">
-                <div className="card p-3 h-100">
-                    <Image src={blog.poster || "./file.svg"} height={272} width={100} className="rounded card-img-top img-fluid" alt={`${blog.title} Adlı Resim`} />
-                    <div className="card-body">
-                        <h5 className="card-title">
-                            {blog.title}
-                        </h5>
-                        <p className="card-text">{
-                            blog.content}</p>
-                    </div>
-                    <div className="card-footer border-top-0">
-                        <Link href={`/blogs/${blog.id}`} className="btn btn-primary">Daha fazlası</Link>
-                    </div>
-                </div>
-            </div>}
-        </>
-    )
-}
+  const myLoader = ({ src }) => {
+    return "https://picsum.photos/200/300";
+  };
+  return (
+    <>
+      {blog && (
+        <div className="col-lg-3 rounded">
+          <div className="card p-3 h-100">
+            <Image
+              src={blog?.poster || "https://picsum.photos/200/300"}
+              height={50}
+              width={150}
+              className="rounded card-img-top img-fluid w-100 h-100"
+              alt={`${blog.title} Adlı Resim`}
+              priority
+            />
+            <div className="card-body">
+              <h5 className="card-title">{blog.title}</h5>
+              <p className="card-text">{blog.content}</p>
+            </div>
+            <div className="card-footer border-top-0">
+              <Link href={`blog/${blog.id}`} className="btn btn-primary">
+                Daha fazlası
+              </Link>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
-export default BlogCard
+export default BlogCard;
