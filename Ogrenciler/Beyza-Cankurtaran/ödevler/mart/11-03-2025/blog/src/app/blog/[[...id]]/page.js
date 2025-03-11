f2b9639e76ed65f9db6c632956f572f9a16cfe53
Beyza Cankurtaran <byzcnkrtrn@gmail.com>
2025-03-12 00:12:36 +0300

    of

--- Ogrenciler/Beyza-Cankurtaran/ödevler/mart/11-03-2025/blog/src/app/blog/[[...id]]/page.js
+++ Ogrenciler/Beyza-Cankurtaran/ödevler/mart/11-03-2025/blog/src/app/blog/[[...id]]/page.js
@@ -1,3 +1,34 @@
+'use client';
+import data from "../../../data/blog.json";
+import { useEffect, useState } from "react";
+
 export default function BlogDetail({ params }) {
-  return <div>Blog ID: {params.name}</div>;
+  const [blog,setBlog]=useState({});
+  //const id={params.id}
+  const ID=params.id;
+  useEffect(() => {
+    if (id) {
+      const IDBlog = data.find(blog => blog.id === Number(id));  
+      setBlog(IDBlog);
+      console.log("idBlog:", IDBlog);  
+    }
+  }, [id]);
+  
+  console.log(blogID);
+  console.log(blog);
+  // if(!blog){
+  //   return <h1 className="text-center mt-5">Blog Bulunamadı!</h1>;
+
+  // }
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center">{blog.title}</h1>
+      <img src={blog.image}alt="Blog image" className="img-fluid mx-auto d-block"/>
+      <p>{blog.author}</p>
+      <p>{blog.content}</p>
+
+    </div>
+
+
+  );
 }
