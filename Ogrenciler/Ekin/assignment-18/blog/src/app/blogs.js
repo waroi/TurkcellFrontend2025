fec278cae8ded62844a7c3d99a192b12a9a41e9a
Ekin Aslan <ekinaslan.js@gmail.com>
2025-03-11 23:04:55 +0300

    Blog CRUD

--- Ogrenciler/Ekin/assignment-18/blog/src/app/blogs.js
+++ Ogrenciler/Ekin/assignment-18/blog/src/app/blogs.js
@@ -1,67 +1,48 @@
-// import { create } from "zustand";
-// import { createStore, useStore } from "zustand";
+import { createStore, useStore } from "zustand";
 
-// import { getBlogs, addBlog as addBlogToServer, editBlog } from "./database";
+import {
+  getBlogs as getBlogsFromServer,
+  addBlog as addBlogToServer,
+  editBlog as editBlogToServer,
+  deleteBlog as deleteBlogFromServer,
+} from "./database";
 
-// const initial = await getBlogs();
-
-// const state = createStore((set) => ({
-//   blogs: initial,
-
-//   addBlog: (blog) => {
-//     console.log("İMDAAAAAAAAAAT");
-//     blog.id = Math.random().toString(36).substring(2);
-
-//     addBlogToServer(blog);
-
-//     return set((state) => ({
-//       blogs: [...state.blogs, blog],
-//     }));
-//   },
-
-//   editBlog: (blog) =>
-//     set((state) => ({
-//       blogs: state.blogs.map((selected) =>
-//         blog.id == selected.id ? blog : selected
-//       ),
-//     })),
-
-//   deleteBlog: (id) =>
-//     set((state) => ({
-//       blogs: state.blogs.filter((blog) => id != blog.id),
-//     })),
-// }));
-
-// const useBookStore = () => useStore(state);
-
-// export default useBookStore;
-
-//////////////////////////////////////////////////////
-
-import { create } from "zustand";
-import { getBlogs, addBlog as addBlogToServer, editBlog } from "./database";
-const useBlogStore2 = create((set) => ({
+const state = createStore((set) => ({
   blogs: [],
-  fetchBlogs: async () => {
-    const blogs = await getBlogs();
-    set({ blogs });
+
+  getBlogs: async () => {
+    set({ blogs: await getBlogsFromServer() });
   },
-  addBlog: (blog) =>
-    set((state) => {
-      blog.id = Math.random().toString(36).substring(2);
-      addBlogToServer(blog);
-      return { blogs: [...state.blogs, blog] };
-    }),
-  editBlog: (blog) =>
+
+  addBlog: (blog) => {
+    blog.id = Math.random().toString(36).substring(2);
+
+    addBlogToServer(blog);
+
+    set((state) => ({
+      blogs: [...state.blogs, blog],
+    }));
+  },
+
+  editBlog: (blog) => {
+    editBlogToServer(blog);
+
     set((state) => ({
       blogs: state.blogs.map((selected) =>
-        blog.id === selected.id ? blog : selected
+        blog.id == selected.id ? blog : selected
       ),
-    })),
-  deleteBlog: (id) =>
+    }));
+  },
+
+  deleteBlog: (id) => {
+    deleteBlogFromServer(id);
+
     set((state) => ({
-      blogs: state.blogs.filter((blog) => id !== blog.id),
-    })),
+      blogs: state.blogs.filter((blog) => id != blog.id),
+    }));
+  },
 }));
 
-export default useBlogStore2;
+const useBookStore = () => useStore(state);
+
+export default useBookStore;
