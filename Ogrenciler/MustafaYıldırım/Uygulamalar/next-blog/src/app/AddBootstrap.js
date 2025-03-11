60f76e15009f32a26c2d4aeaa9be02896cb7ee4a
Mustafa Yıldırım <69580632+mustafayildirim57@users.noreply.github.com>
2025-03-11 14:56:15 +0300

    starts: blogs

---
+++ Ogrenciler/MustafaYıldırım/Uygulamalar/next-blog/src/app/AddBootstrap.js
@@ -0,0 +1,12 @@
+//  AddBootstrap.js
+
+"use client";
+
+import { useEffect } from "react";
+
+export default function AddBootstrap() {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap.bundle.js");
+  }, []);
+  return <></>;
+}
