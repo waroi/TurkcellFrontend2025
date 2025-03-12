0981f79030fcfbd4f05b759240bf57f04c2a2fb4
Fahri Coşkun <fahricosskun@gmail.com>
2025-03-12 03:19:06 +0300

    update: BlogList, Blog, AddBlog pages, blog crud add

--- Ogrenciler/Fahri/Dersler/Ders-27-NextJS/my-app/src/app/layout.js
+++ Ogrenciler/Fahri/Dersler/Ders-27-NextJS/my-app/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import StoreProvider from "./Provider/StoreProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,10 +19,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
-    </html>
+    <StoreProvider>
+      <html lang="en">
+        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+          {children}
+        </body>
+      </html>
+    </StoreProvider>
   );
 }
