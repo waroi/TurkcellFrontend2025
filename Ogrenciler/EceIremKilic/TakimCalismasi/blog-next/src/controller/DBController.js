1644c3ff4e98eaa6ba7f217e2cd34f296b2082f5
Mustafa Fatih Gündüz <mustafatihgunduz@gmail.com>
2025-03-14 01:17:18 +0300

    feature: Firebase Data's added - All posts coming from firebase
    Tüm post değerlerimiz artık firebase üzerinden geliyor ve üzerinden işlemler yapılıyor

--- Ogrenciler/EceIremKilic/TakimCalismasi/blog-next/src/controller/DBController.js
+++ Ogrenciler/EceIremKilic/TakimCalismasi/blog-next/src/controller/DBController.js
@@ -51,4 +51,3 @@ export const checkIsHeAdmin = async () => {
 		return null;
 	}
 };
-
--- Ogrenciler/EceIremKilic/TakimCalismasi/blog-next/src/store/useBlogStore.js
+++ Ogrenciler/EceIremKilic/TakimCalismasi/blog-next/src/store/useBlogStore.js
@@ -1,51 +1,41 @@
 import { create } from "zustand";
-import data from "../../data/data.json";
+import {
+	collection,
+	doc,
+	getDoc,
+	getDocs,
+	query,
+	setDoc,
+	where,
+} from "firebase/firestore";
+import { db } from "../firebase_config";
 
 const useBlogStore = create((set) => ({
-  posts: [...data.posts],
-  id: 17,
+
+  posts: [],
   getPosts: async () => {
-    const response = await fetch(`http://localhost:3000/posts`);
-    const posts = await response.json();
-    set({ posts });
+    try {
+      const q = query(collection(db, "posts"));
+      const snaps = await getDocs(q);
+      const allPosts = snaps.docs.forEach((post) => set((state) => ({
+        posts: [...state.posts,post.data()]
+      })) )
+      // for (const doc of snaps.docs) {
+      //   set((state) => ({
+      //     posts: [...state.posts, doc.data()],
+      //   }));
+      // }
+    } catch (error) {
+      console.log("fetchAllPosts DBController Error", error);
+    }
   },
   getPostById: async (id) => {
-    const response = await fetch(`http://localhost:3000/posts/${id}`);
-    return response.json();
   },
   addPost: async (newPost) => {
-    set((state) => ({
-      posts: [...state.posts, { id: state.id, ...newPost }],
-      id: state.id + 1,
-    }));
-    const response = await fetch(`http://localhost:3000/posts`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newPost),
-    });
-    return response.json();
   },
   updatePost: async (id, updatedPost) => {
-    set((state) => ({
-      posts: state.posts.map((post) =>
-        post.id === id ? { ...post, ...updatedPost } : post
-      ),
-    }));
-    const response = await fetch(`http://localhost:3000/posts/${id}`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(updatedPost),
-    });
-    return response.json();
   },
   deletePost: async (id) => {
-    set((state) => ({
-      posts: state.posts.filter((post) => post.id !== id),
-    }));
-    const response = await fetch(`http://localhost:3000/posts/${id}`, {
-      method: "DELETE",
-    });
-    return response.json();
   },
 }));
 
