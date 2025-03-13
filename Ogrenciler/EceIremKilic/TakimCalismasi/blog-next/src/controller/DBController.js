4b1782107933eeb3bd9c19e551e9c55780862992
eceiremklc <eceiremklc@gmail.com>
2025-03-14 01:36:43 +0300

    fix file structure

--- Ogrenciler/EceIremKilic/TakimCalismasi/blog-next/src/controller/DBController.js
+++ Ogrenciler/EceIremKilic/TakimCalismasi/blog-next/src/controller/DBController.js
@@ -1,53 +1,53 @@
 import {
-	collection,
-	doc,
-	getDoc,
-	getDocs,
-	query,
-	setDoc,
-	where,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  setDoc,
+  where,
 } from "firebase/firestore";
 import { auth, db } from "../firebase_config";
 
 export const readUser = async (userID) => {
-	try {
-		const userRef = doc(db, "users", userID);
-		const userSnap = await getDoc(userRef);
-		if (userSnap.exists()) {
-			return userSnap.data();
-		} else {
-			console.log("DBController readUser error!");
-			return null;
-		}
-	} catch (error) {
-		console.error("DBController readUser error!:", error);
-		return null;
-	}
+  try {
+    const userRef = doc(db, "users", userID);
+    const userSnap = await getDoc(userRef);
+    if (userSnap.exists()) {
+      return userSnap.data();
+    } else {
+      console.log("DBController readUser error!");
+      return null;
+    }
+  } catch (error) {
+    console.error("DBController readUser error!:", error);
+    return null;
+  }
 };
 export const saveUser = async (user) => {
-	try {
-		const userRef = doc(db, "users", user.uid);
-		await setDoc(userRef, user);
-		console.log("DBController saveUser error!:", user);
-		return user;
-	} catch (error) {
-		console.error("DBController saveUser error!:", error.message);
-		return null;
-	}
+  try {
+    const userRef = doc(db, "users", user.uid);
+    await setDoc(userRef, user);
+    console.log("DBController saveUser error!:", user);
+    return user;
+  } catch (error) {
+    console.error("DBController saveUser error!:", error.message);
+    return null;
+  }
 };
 
 export const checkIsHeAdmin = async () => {
-	try {
-		const q = query(collection(db, "users"), where("isAdmin", "==", true));
-		const snaps = await getDocs(q);
-		const currentID = auth.currentUser.uid;
-		for (const doc of snaps.docs) {
-			if (doc.id === currentID) {
-				return doc.data().isAdmin;
-			}
-		}
-	} catch (error) {
-		console.log("checkIsHeAdmin DBController Error", error);
-		return null;
-	}
+  try {
+    const q = query(collection(db, "users"), where("isAdmin", "==", true));
+    const snaps = await getDocs(q);
+    const currentID = auth.currentUser.uid;
+    for (const doc of snaps.docs) {
+      if (doc.id === currentID) {
+        return doc.data().isAdmin;
+      }
+    }
+  } catch (error) {
+    console.log("checkIsHeAdmin DBController Error", error);
+    return null;
+  }
 };
--- Ogrenciler/EceIremKilic/TakimCalismasi/blog-next/src/store/useBlogStore.js
+++ Ogrenciler/EceIremKilic/TakimCalismasi/blog-next/src/store/useBlogStore.js
@@ -1,25 +1,26 @@
 import { create } from "zustand";
 import {
-	collection,
-	doc,
-	getDoc,
-	getDocs,
-	query,
-	setDoc,
-	where,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  setDoc,
+  where,
 } from "firebase/firestore";
 import { db } from "../firebase_config";
 
 const useBlogStore = create((set) => ({
-
   posts: [],
   getPosts: async () => {
     try {
       const q = query(collection(db, "posts"));
       const snaps = await getDocs(q);
-      const allPosts = snaps.docs.forEach((post) => set((state) => ({
-        posts: [...state.posts,post.data()]
-      })) )
+      const allPosts = snaps.docs.forEach((post) =>
+        set((state) => ({
+          posts: [...state.posts, post.data()],
+        }))
+      );
       // for (const doc of snaps.docs) {
       //   set((state) => ({
       //     posts: [...state.posts, doc.data()],
@@ -29,14 +30,10 @@ const useBlogStore = create((set) => ({
       console.log("fetchAllPosts DBController Error", error);
     }
   },
-  getPostById: async (id) => {
-  },
-  addPost: async (newPost) => {
-  },
-  updatePost: async (id, updatedPost) => {
-  },
-  deletePost: async (id) => {
-  },
+  getPostById: async (id) => {},
+  addPost: async (newPost) => {},
+  updatePost: async (id, updatedPost) => {},
+  deletePost: async (id) => {},
 }));
 
 export default useBlogStore;
