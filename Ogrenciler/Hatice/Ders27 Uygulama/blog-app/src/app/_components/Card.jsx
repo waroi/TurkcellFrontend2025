544238442ab82877f5a163275bebd9cb71345a22
elinoza <hillcakmak@gmail.com>
2025-03-15 00:52:27 +0300

    card,cardfooter,button, some helper functions refactored.Input left,navbar left, store can be added for user management and db can be added

--- Ogrenciler/Hatice/Ders27 Uygulama/blog-app/src/app/_components/Card.jsx
+++ Ogrenciler/Hatice/Ders27 Uygulama/blog-app/src/app/_components/Card.jsx
@@ -1,59 +1,58 @@
-import * as React from 'react';
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
+"use client";
+import * as React from "react";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 import { myLoader } from "@/utils/functions";
-import Link from 'next/link';
-import  Button  from './Button';
-import { toast } from 'react-toastify';
-import clsx from 'clsx';
+import Link from "next/link";
+import Button from "./Button";
+import { toast } from "react-toastify";
+import clsx from "clsx";
 
-function Card({ poster, title, content, author, date, id, layout="horizontal", deleteFunction, revalidateFunction, deleteSuccessMessage="Başarıyla silindi" }) {
+function Card({
+  poster,
+  title,
+  content,
+  author,
+  date,
+  id,
+  layout = "horizontal",
+  deleteFunction,
+  revalidateFunction,
+  deleteSuccessMessage = "Başarıyla silindi",
+  children,
+}) {
   return (
-    <div className="container mt-4">
-      <div className="card mb-3 border-0">
-        <div className="row g-0">
-          <div className= {clsx("diğer classlar", layout==="horizontal" && "col-12" )}>
-            <Image
-              src={"/noImage.png"}
-              height={50}
-              width={150}
-              className="rounded card-img-top img-fluid w-100 h-100"
-              alt={`${title} Adlı Resim`}
-              loader={() => myLoader(poster)}
-              priority
-            />
+    <div className="card mb-3 border-0">
+      <div className="row g-0">
+        <div
+          className={clsx(
+            layout === "horizontal" && "col-md-4",
+            layout === "vertical" && "col-12"
+          )}
+        >
+          <Image
+            src={"/noImage.png"}
+            height={50}
+            width={150}
+            className="card-img-top img-fluid w-100 h-100"
+            alt={`${title} Adlı Resim`}
+            loader={() => myLoader(poster)}
+            priority
+          />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body d-flex flex-column h-100 ">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{content}</p>
+            <p className="card-text justify-self-end">
+              <small className="text-body-secondary">
+                Yazar: {author} | {date && new Date(date).toLocaleDateString()}
+              </small>
+            </p>
           </div>
-          <div className="col-md-8">
-            <div className="card-body d-flex flex-column h-100 ">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">{content}</p>
-              <p className="card-text justify-self-end">
-                <small className="text-body-secondary">
-                  Yazar: {author} |{" "}
-                  {date && new Date(date).toLocaleDateString()}
-                </small>
-              </p>
-            </div>
-          </div>
-          <div className="card-footer border-top-0 bg-transparent ">
-              <Link href={`blog/${id}`} className="btn btn-primary">
-                Daha fazlası
-              </Link>
-              <Button
-                className="btn btn-danger ms-2"
-                onClick={async () => {
-                  const result = await deleteFunction(id);
-                  if (result) {
-                    await revalidateFunction();
-                    toast.success(deleteSuccessMessage);
-                  }
-                }}
-              >
-                Sil
-              </Button>
-            </div>
         </div>
       </div>
+      {children}
     </div>
   );
 }
