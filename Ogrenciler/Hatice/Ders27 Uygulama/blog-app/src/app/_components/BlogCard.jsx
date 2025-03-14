970c8b9a0847eddbafd1d7a0cc59d2e5662d951d
Hazal Kütküt <hazalktt@gmail.com>
2025-03-14 21:27:54 +0300

    update: card.jsx,

--- Ogrenciler/Hatice/Ders27 Uygulama/blog-app/src/app/_components/BlogCard.jsx
+++ Ogrenciler/Hatice/Ders27 Uygulama/blog-app/src/app/_components/BlogCard.jsx
@@ -5,46 +5,14 @@ import { myLoader } from "@/utils/functions";
 import Button from "./Button";
 import { deleteBlog } from "@/utils/services/helpers";
 import { toast } from "react-toastify";
+import Card from "./Card";
 
 const BlogCard = ({ blog, getBlogs }) => {
   return (
     <>
       {blog && (
         <div className="col-lg-4 rounded">
-          <div className="card p-3 h-100">
-            <Image
-              src={"/noImage.png"}
-              height={50}
-              width={150}
-              className="rounded card-img-top img-fluid w-100 h-100"
-              alt="Adlı Resim"
-              loader={() => myLoader(blog?.poster)}
-              priority
-            />
-            <div className="card-body bg-transparent">
-              <h5 className="card-title fw-bold">{blog.title}</h5>
-              <p className="card-text text-muted">
-                {blog.content.substring(0, 100)}
-              </p>
-            </div>
-            <div className="card-footer border-top-0 bg-transparent ">
-              <Link href={`blog/${blog.id}`} className="btn btn-primary">
-                Daha fazlası
-              </Link>
-              <Button
-                className="btn btn-danger ms-2"
-                onClick={async () => {
-                  const result = await deleteBlog(blog?.id);
-                  if (result) {
-                    await getBlogs();
-                    toast.success("Blog başarıyla silindi.");
-                  }
-                }}
-              >
-                Sil
-              </Button>
-            </div>
-          </div>
+          <Card key={blog.id} blog={blog} revalidateFunction={getBlogs} date={blog.date} poster={blog.poster} content={blog.content} author={blog.author} deleteFunction={deleteBlog} />
         </div>
       )}
     </>
