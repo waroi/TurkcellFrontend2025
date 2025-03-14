26376c2b65d5e16a1632e28212859b8fd6165a0b
Hazal Kütküt <hazalktt@gmail.com>
2025-03-14 16:07:56 +0300

    update

--- Ogrenciler/Hatice/Ders27 Uygulama/blog-app/src/app/(auth)/register/page.jsx
+++ Ogrenciler/Hatice/Ders27 Uygulama/blog-app/src/app/(auth)/register/page.jsx
@@ -1,47 +1,36 @@
 "use client";
-import React, { useState } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "@/utils/firebaseConfig";
-import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { toast } from "react-toastify";
+import { auth } from "../../../utils/firebaseConfig";
+import { login, register } from "../actions";
+import { useActionState } from "react";
+
+const initialState = {
+  message: "",
+};
 
 const Register = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const router = useRouter();
-
-  const handleRegister = async (e) => {
-    e.preventDefault();
-    setError("");
-
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
-      router.push("/login");
-    } catch (err) {
-      setError("Kayıt başarısız: " + err.message);
-    }
-  };
+  const [state, formAction, pending] = useActionState(register, initialState);
+  console.log(state?.message);
 
   return (
     <div className="auth">
       <h2>Kayıt Ol</h2>
-      <form onSubmit={handleRegister} className="input-div">
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Şifre"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        {error && <p className="text-danger">{error}</p>}
-        <button type="submit" className="button-div">Kayıt Ol</button>
+      <form action={formAction} className="input-div">
+        <input name="email" type="text" placeholder="Email adres" required />
+        <input name="password" type="password" placeholder="Şifre" required />
+        <p className="text-warning" aria-live="polite">
+          {state?.message}
+        </p>
+        <div>
+          <button disabled={pending} type="submit" className="button-div">
+            Kayıt ol
+          </button>
+        </div>
       </form>
     </div>
   );
