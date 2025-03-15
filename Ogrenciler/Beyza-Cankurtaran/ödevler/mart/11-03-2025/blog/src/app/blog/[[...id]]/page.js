bf1d688c5d58aaf4e4405ea07ae5dc30f25ccb4a
Sedanur CEYLAN <sdnrcyln2@gmail.com>
2025-03-15 14:26:43 +0300

    update css

--- Ogrenciler/Beyza-Cankurtaran/ödevler/mart/11-03-2025/blog/src/app/blog/[[...id]]/page.js
+++ Ogrenciler/Beyza-Cankurtaran/ödevler/mart/11-03-2025/blog/src/app/blog/[[...id]]/page.js
@@ -1,34 +1,36 @@
-'use client';
+"use client";
 
-import data from '../../../data/blog.json';
-import { useEffect, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../../globals.css';
-import DetailCard from '@/component/DetailCard';
+import data from "../../../data/blog.json";
+import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../../globals.css";
+import DetailCard from "@/component/DetailCard";
+import { use } from "react"; // 'use' fonksiyonunu ekledik
 
 export default function BlogDetail({ params }) {
   const [blog, setBlog] = useState(null);
 
-  const { id } = params;
+  // 'params' nesnesini 'use' ile çözümleyin
+  const { id } = use(params); // params'i 'use' ile çözümleyin
 
   useEffect(() => {
     if (id) {
       const IDBlog = data.find((blog) => blog.id === Number(id));
 
       setBlog(IDBlog);
-      console.log('IDBlog:', IDBlog);
+      console.log("IDBlog:", IDBlog);
     }
   }, [id]);
 
   if (!blog) {
     return (
-      <div className='container my-5 text-center'>
-        <div className='alert alert-warning py-4'>
-          <h1 className='display-5'>Blog Bulunamadı!</h1>
-          <p className='lead mt-3'>
+      <div className="container my-5 text-center">
+        <div className="alert alert-warning py-4">
+          <h1 className="display-5">Blog Bulunamadı!</h1>
+          <p className="lead mt-3">
             The requested blog post could not be found.
           </p>
-          <a href='/' className='btn btn-outline-warning mt-3'>
+          <a href="/" className="btn btn-outline-warning mt-3">
             Ana Sayfaya Dön
           </a>
         </div>
@@ -37,7 +39,7 @@ export default function BlogDetail({ params }) {
   }
 
   return (
-    <div className='container my-5 blog-detail'>
+    <div className="container my-5 blog-detail">
       <DetailCard blog={blog} />
     </div>
   );
