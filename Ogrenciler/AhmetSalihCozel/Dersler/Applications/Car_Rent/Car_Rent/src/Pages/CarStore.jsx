590b290dba49e51bb39ff44725c78cb1349fdb9f
ahmetsalihcozel <ahmetsalihcozel@gmail.com>
2025-03-18 17:28:16 +0300

    Update CarStore.jsx

--- Ogrenciler/AhmetSalihCozel/Dersler/Applications/Car_Rent/Car_Rent/src/Pages/CarStore.jsx
+++ Ogrenciler/AhmetSalihCozel/Dersler/Applications/Car_Rent/Car_Rent/src/Pages/CarStore.jsx
@@ -2,21 +2,39 @@ import React from 'react'
 import carStore from '../Store/carStore'
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
-
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 
 const CarStore = () => {
   const {car} = carStore((state)=>state)
   return (
-    <div>
-      <Container maxWidth="lg">
-        <Grid container spacing={2}>
-          <Grid item lg={6} s={12}>
-            
-          </Grid>
-        </Grid>
-      </Container>
-    </div>
+    car.map(()=>{
+      <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Favorilere Ekle</Button>
+        <Button size="small">Incele</Button>
+      </CardActions>
+    </Card>
+    })
   )
 }
 
