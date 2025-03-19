dc4657a28e8f15da62cc4b7bb09b8f40552b5ea2
Berfin Ela Kılıç <77438033+Elakilic@users.noreply.github.com>
2025-03-19 20:54:58 +0300

    Formik form start

---
+++ Ogrenciler/BerfinElaKilic/Homework/Formik-Odev/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import AdayFormu from './components/AdayFormu'
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+
+  return (
+    <>
+    <AdayFormu/>
+    </>
+  )
+}
+
+export default App
