43208f48a0ac98de6d57f524730cfe00920c6097
boranaybike <boranaybike.dev@gmail.com>
2025-03-20 00:37:17 +0300

    Add Folder Structure

---
+++ Ogrenciler/Aybike/Applications/resume-app/src/components/layout/Layout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+
+const Layout = () => {
+	return (
+		<>
+			<Header />
+			<main>
+				<Outlet />
+			</main>
+			<Footer />
+		</>
+	);
+};
+
+export default Layout;
