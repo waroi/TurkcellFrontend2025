772a36aea4c065cc109d6fbcf4aa42babe5453f6
ahmetsalihcozel <ahmetsalihcozel@gmail.com>
2025-03-21 10:07:20 +0300

    formik css updates

--- Ogrenciler/Hatice/Ders28 Uygulama/job-app/src/components/Footer.jsx
+++ Ogrenciler/Hatice/Ders28 Uygulama/job-app/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-      <footer className="bg-dark-blue w-100 py-5 fixed-bottom">
+      <footer className="bg-primary w-100 d-flex justify-content-between align-items-center fixed-bottom">
         <div className="container text-white text-center">
           Ahmet Salih, Hatice, Zeynep | &copy; 2025
         </div>
