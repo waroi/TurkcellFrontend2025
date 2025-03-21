895164332255cdce1d17a928491344a3d5952c27
Mustafa Yıldırım <69580632+mustafayildirim57@users.noreply.github.com>
2025-03-21 15:55:31 +0300

    add: add rate degreans and form elements

--- Ogrenciler/Aybike/Applications/resume-app/src/services/ApplicationService.jsx
+++ Ogrenciler/Aybike/Applications/resume-app/src/services/ApplicationService.jsx
@@ -1,41 +1,61 @@
-import { addDoc, collection, doc, getDoc, getDocs } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
 import { db } from "../firebase/Firebase";
 
 export class ApplicationService {
-    static async getApplications() {
-        const querySnapshot = await getDocs(collection(db, "applications"));
-        const applicationsData = querySnapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-        }));
-        return applicationsData
+  static async getApplications() {
+    const querySnapshot = await getDocs(collection(db, "applications"));
+    const applicationsData = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    return applicationsData;
+  }
+
+  static async addApplication(values) {
+    try {
+      if (!values) throw new Error("Geçersiz başvuru verisi kankaaa");
+      const docRef = await addDoc(collection(db, "applications"), {
+        ...values,
+        rate: 0,
+      });
+      return { success: true, message: "Başvuru başarıyla eklendi broo :))" };
+    } catch (error) {
+      console.error("Başvuru eklerken boom-->", error);
+      return { success: false, message: error };
     }
+  }
 
-    static async addApplication(values) {
-        try {
-            if (!values) throw new Error("Geçersiz başvuru verisi kankaaa")
-            const docRef = await addDoc(collection(db, "applications"), { ...values, rate: 0 })
-            return { success: true, message: "Başvuru başarıyla eklendi broo", }
+  static async getApplicationById(applicationId) {
+    try {
+      const docRef = doc(db, "applications", applicationId);
+      const docSnap = await getDoc(docRef);
 
-        } catch (error) {
-            console.error("Başvuru eklerken boom-->", error);
-            return { success: false, message: error };
-        }
+      if (docSnap.exists()) {
+        return { success: true, data: { id: docSnap.id, ...docSnap.data() } };
+      } else {
+        return { success: false, message: "Başvuru bulunamadı!" };
+      }
+    } catch (error) {
+      console.error("Başvuru detayları alınırken hata oluştu:", error);
+      return { success: false, message: error };
     }
+  }
+  static async updateApplicationRate(applicationId, newRate) {
+    try {
+      const docRef = doc(db, "applications", applicationId);
+      await updateDoc(docRef, { rate: newRate });
 
-    static async getApplicationById(applicationId) {
-        try {
-            const docRef = doc(db, "applications", applicationId)
-            const docSnap = await getDoc(docRef)
-
-            if (docSnap.exists()) {
-                return { success: true, data: { id: docSnap.id, ...docSnap.data() } }
-            } else {
-                return { success: false, message: "Başvuru bulunamadı!" }
-            }
-        } catch (error) {
-            console.error("Başvuru detayları alınırken hata oluştu:", error)
-            return { success: false, message: error }
-        }
+      return { success: true, message: "Puan başarıyla güncellendi" };
+    } catch (error) {
+      console.error("Puan güncellenirken hata oluştu:", error);
+      return { success: false, message: error };
     }
+  }
 }
