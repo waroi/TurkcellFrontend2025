b369b681ba90da2bd8dd4b2f5a0982550397f2a7
elinoza <hillcakmak@gmail.com>
2025-03-22 22:04:43 +0300

    update:useAuth

--- Ogrenciler/BerfinElaKilic/Homework/Formik-Odev/src/utils/firebaseConfig.js
+++ Ogrenciler/BerfinElaKilic/Homework/Formik-Odev/src/utils/firebaseConfig.js
@@ -14,6 +14,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+console.log(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
