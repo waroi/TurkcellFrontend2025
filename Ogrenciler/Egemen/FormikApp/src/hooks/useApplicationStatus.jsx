4647b2f23f7a64eb9b3da148992a109b7d9732c6
eceiremklc <eceiremklc@gmail.com>
2025-03-28 02:55:56 +0300

    some code optimizations

--- Ogrenciler/Egemen/FormikApp/src/hooks/useApplicationStatus.jsx
+++ Ogrenciler/Egemen/FormikApp/src/hooks/useApplicationStatus.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback } from "react";
 import { updateAppStatus, updateMessage } from "../../firebase/dbController";
 
 const useApplicationStatus = (setApps) => {
@@ -13,40 +13,40 @@ const useApplicationStatus = (setApps) => {
           newStatus = "Test";
           userMessage = "Testi Çöz";
           adminMessage = "Test aşamasında";
+          application.status = "Test";
 
           break;
         case "Test":
           newStatus = "Test Kontrol";
           userMessage = "Testiniz değerlendiriliyor.";
           adminMessage = "Mülakat aşamasını başlat";
+          application.status = "Test Kontrol";
           break;
         case "Test Kontrol":
           newStatus = "Mülakat";
           userMessage = "Mülakata davet edildiniz.";
           adminMessage = "Mülakat aşamasına geçildi";
+          application.status = "Mülakat";
 
           break;
 
-        case "Red":
-          newStatus = "Red";
-          userMessage = "Başvurunuz reddedildi.";
-          break;
         default:
           return;
       }
 
-      setApps((prevApps) =>
-        prevApps.map((app) =>
-          app.id === application.id
-            ? {
-                ...app,
-                status: newStatus,
-                userMessage: userMessage,
-                adminMessage: adminMessage,
-              }
-            : app
-        )
-      );
+      // setApps((prevApps) => {
+      //   console.log("prevApps", prevApps);
+      //   prevApps.map((app) =>
+      //     app.id === application.id
+      //       ? {
+      //           ...app,
+      //           status: newStatus,
+      //           userMessage: userMessage,
+      //           adminMessage: adminMessage,
+      //         }
+      //       : app
+      //   );
+      // });
 
       try {
         await updateAppStatus({ ...application, status: newStatus });
