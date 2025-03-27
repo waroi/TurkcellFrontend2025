0549b00d74e4cc33b79d86724b4dc5b77131d9bd
Fahri Coşkun <fahricosskun@gmail.com>
2025-03-27 21:15:44 +0300

    update: page css files add

--- Ogrenciler/MustafaFatihGunduz/EmployApp/empapp/src/pages/AdminPanel.jsx
+++ Ogrenciler/MustafaFatihGunduz/EmployApp/empapp/src/pages/AdminPanel.jsx
@@ -1,3 +1,5 @@
+import "../styles/variables.css";
+import styles from "./AdminPanel.module.css";
 import { auth } from "../../firebase_config";
 import useFetchApplication from "../store/useFetchApplication";
 import useAdminPanel from "../store/useAdminPanel";
@@ -23,15 +25,15 @@ const AdminPanel = () => {
     handleReject,
   } = useAdminPanel();
 
-  const {showModal,handleCloseModal,handleModal} = useAdminTest();
+  const { showModal, handleCloseModal, handleModal } = useAdminTest();
 
   const filteredApplications = getFilteredApplications(applications);
 
   if (isLoading) {
     return (
-      <div className={`container mt-4`}>
+      <div className={styles.adminContainer}>
         <div className="text-center">
-          <div className="spinner-border spinner" role="status">
+          <div className="spinner-border" role="status">
             <span className="visually-hidden">Yükleniyor...</span>
           </div>
           <p className="mt-2">Yükleniyor...</p>
@@ -41,11 +43,11 @@ const AdminPanel = () => {
   }
 
   return (
-    <>
+    <div className={styles.adminContainer}>
       <div className="container">
-        <div className=" d-flex justify-content-between align-items-center mb-4">
-          <h1 className="fw-bolder">Başvuruları Yönet</h1>
-          <Link to="/" className="btn btn-secondary ">
+        <div className={styles.header}>
+          <h1 className={styles.title}>Başvuruları Yönet</h1>
+          <Link to="/" className={styles.backButton}>
             <i className="bi bi-arrow-left"></i> Başvuru Sayfasına Dön
           </Link>
         </div>
@@ -55,19 +57,19 @@ const AdminPanel = () => {
         </p>
 
         {!auth.currentUser ? (
-          <div className="text-center mb-4">
+          <div className={styles.messageContainer}>
             <p className="alert alert-info">
               Başvuruları görüntülemek için lütfen giriş yapın
             </p>
             <button
-              className="btn btn-primary"
+              className={styles.loginButton}
               onClick={handleLoginAndCheckAdmin}
             >
               Giriş yap
             </button>
           </div>
         ) : !isAdmin ? (
-          <div className="text-center mb-4">
+          <div className={styles.messageContainer}>
             <p className="alert alert-warning">
               Bu sayfa sadece admin tarafından görüntülenebilir
             </p>
@@ -78,7 +80,7 @@ const AdminPanel = () => {
               <div className="col-md-12">
                 <input
                   type="text"
-                  className="form-control p-3 mb-4 border rounded shadow-sm"
+                  className={styles.searchInput}
                   placeholder="İsim veya yetenek girin"
                   value={searchTerm}
                   onChange={(e) => setSearchTerm(e.target.value)}
@@ -88,70 +90,71 @@ const AdminPanel = () => {
             <div className="row">
               {filteredApplications.map((app) => (
                 <div key={app.id} className="col-md-4 mb-4">
-                  <div className={`card`}>
-                    <div className="card-body">
-                      <h5 className="card-title text-dark fw-semibold border-bottom pb-2 mb-3">
+                  <div className={styles.applicationCard}>
+                    <div className={styles.cardBody}>
+                      <h5 className={styles.cardTitle}>
                         {app.name} {app.lastName}
                       </h5>
-                      <p className="card-text">
+                      <p className={styles.cardText}>
                         <strong>Adres:</strong> {app.adressFirst},{" "}
                         {app.adressSecond}
                       </p>
-                      <p className="card-text">
+                      <p className={styles.cardText}>
                         <strong>Şehir:</strong> {app.city} - {app.province}
                       </p>
-                      <p className="card-text">
+                      <p className={styles.cardText}>
                         <strong>Posta Kodu:</strong> {app.postCode}
                       </p>
-                      <p className="card-text">
+                      <p className={styles.cardText}>
                         <strong>Ülke:</strong> {app.country}
                       </p>
-                      <p className="card-text">
+                      <p className={styles.cardText}>
                         <strong>Telefon:</strong> {app.phoneNumber}
                       </p>
-                      <p className="card-text">
+                      <p className={styles.cardText}>
                         <strong>Email:</strong> {app.email}
                       </p>
-                      <p className="card-text">
+                      <p className={styles.cardText}>
                         <strong>Doğum Tarihi:</strong> {app.birthday}
                       </p>
-                      <p className="card-text">
+                      <p className={styles.cardText}>
                         <strong>Türk mü?</strong>{" "}
                         {app.isTurkish ? "Evet" : "Hayır"}
                       </p>
                       {app.skills && (
-                        <p className="card-text">
-                          <strong>Yetenekler:</strong>{" "}
-                          <span className="d-flex flex-wrap gap-2 mt-2">
+                        <p className={styles.cardText}>
+                          <strong>Yetenekler:</strong>
+                          <span className={styles.skillsContainer}>
                             {app.skills.split(",").map((skill, index) => (
-                              <span
-                                key={index}
-                                className=" badge bg-dark rounded-pill"
-                              >
+                              <span key={index} className={styles.skillBadge}>
                                 {skill.trim()}
                               </span>
                             ))}
                           </span>
                         </p>
                       )}
-                      <p className="card-text">
+                      <p className={styles.cardText}>
                         <strong>Üniversite:</strong> {app.university}
                       </p>
-                      <p className="card-text">
+                      <p className={styles.cardText}>
                         <strong>Mezun mu?</strong>{" "}
                         {app.isGraduate ? "Evet" : "Hayır"}
                       </p>
                     </div>
-                    <div className="card-footer gap-4 d-flex justify-content-center">
+                    <div className={styles.cardFooter}>
                       <button
-                        className="btn btn-success"
+                        className={styles.approveButton}
                         onClick={() => handleModal(app)}
                       >
                         Onayla
                       </button>
-                      <AdminTestModal show={showModal} handleClose={handleCloseModal} app={app} />
+                      <AdminTestModal
+                        show={showModal}
+                        handleClose={handleCloseModal}
+                        app={app}
+                      />
                       <button
-                        className="btn btn-danger"
+                        className={styles.rejectButton}
                         onClick={() => handleReject(app)}
                       >
                         Reddet
@@ -166,7 +169,7 @@ const AdminPanel = () => {
 
         <SignInModal show={show} handleClose={handleClose} />
       </div>
-    </>
+    </div>
   );
 };
 
