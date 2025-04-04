1e32af32a2325d64eacfe02005179072b4b8135d
Cansu126 <cevikcnsu126@gmail.com>
2025-04-04 17:01:42 +0300

    update

--- Ogrenciler/Cansu/Ders-38/trendora-project/trendora-project/src/store/useThemeStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const useThemeStore = create((set) => ({
-  theme: "light",
-  toggleTheme: () => set((state) => ({ theme: state.theme === "light" ? "dark" : "light" })),
-}));
-
-export default useThemeStore;
