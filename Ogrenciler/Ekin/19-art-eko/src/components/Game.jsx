8870cd976b02e8aad7b6b1b24b7c2251c2f685ce
Ekin Aslan <ekinaslan.js@gmail.com>
2025-04-07 11:07:57 +0300

    Canvas Players

--- Ogrenciler/Ekin/19-art-eko/src/components/Game.jsx
+++ Ogrenciler/Ekin/19-art-eko/src/components/Game.jsx
@@ -1,21 +1,53 @@
 import Players from "#/Players";
 import Canvas from "#/Canvas";
 import Chat from "#/Chat";
+import { useState } from "react";
 
-export default function Home({ setLoading }) {
+export default function Game() {
+  const [p, setP] = useState([
+    { id: "bilmemne", name: "Ekin Aslan", score: 28 },
+    { id: "user1", name: "Ahmet Yılmaz", score: 23 },
+    { id: "user2", name: "Ayşe Demir", score: 15 },
+    { id: "user3", name: "Mehmet Can", score: 55 },
+    { id: "user4", name: "Zeynep Kaya", score: 19 },
+    { id: "user5", name: "Fatma Öz", score: 21 },
+    { id: "user6", name: "Emre Aydın", score: 24 },
+    { id: "user7", name: "Selin Kurt", score: 16 },
+    { id: "user8", name: "Okan Arslan", score: 18 },
+    { id: "user9", name: "Burcu Şen", score: 22 },
+    { id: "user10", name: "Cem Uslu", score: 20 },
+    { id: "user11", name: "Hülya Çelik", score: 24 },
+    { id: "user12", name: "Mert Yüce", score: 25 },
+    { id: "user13", name: "Gizem Tan", score: 14 },
+    { id: "user14", name: "Tolga Er", score: 26 },
+  ]);
+
+  function DEB() {
+    setP([
+      { id: "bilmemne", name: "Ekin Aslan", score: 57 },
+      { id: "user1", name: "Ahmet Yılmaz", score: 23 },
+      { id: "user2", name: "Ayşe Demir", score: 15 },
+      { id: "user3", name: "Mehmet Can", score: 57 },
+      { id: "user4", name: "Zeynep Kaya", score: 19 },
+      { id: "user5", name: "Fatma Öz", score: 21 },
+      { id: "user6", name: "Emre Aydın", score: 24 },
+      { id: "user7", name: "Selin Kurt", score: 16 },
+      { id: "user8", name: "Okan Arslan", score: 18 },
+      { id: "user9", name: "Burcu Şen", score: 22 },
+      { id: "user10", name: "Cem Uslu", score: 20 },
+      { id: "user11", name: "Hülya Çelik", score: 24 },
+      { id: "user12", name: "Mert Yüce", score: 25 },
+      { id: "user13", name: "Gizem Tan", score: 14 },
+      { id: "user14", name: "Tolga Er", score: 26 },
+    ]);
+  }
+
   return (
     <>
-      <Players
-        players={[
-          {
-            id: "bilmemne",
-            name: "Ekin Aslan",
-            score: 17,
-          },
-        ]}
-      />
+      <Players players={p} />
       <Canvas word="geçenşağı" />
-      <Chat />
+      {/* <Chat /> */}
+      <button onClick={DEB}>a</button>
     </>
   );
 }
--- Ogrenciler/Ekin/19-art-eko/src/components/Players.jsx
+++ Ogrenciler/Ekin/19-art-eko/src/components/Players.jsx
@@ -1,7 +1,50 @@
-export default function Players() {
+import { useEffect, useState } from "react";
+
+export default function Players({ players }) {
+  const [placement, setPlacement] = useState([]);
+  const [profiles, setProfiles] = useState({});
+
+  useEffect(() => {
+    setPlacement([
+      ...players
+        .sort((current, next) => next.score - current.score)
+        .map((player, placement) => ({ ...player, placement })),
+    ]);
+
+    players.map((player) =>
+      setProfiles((profiles) => {
+        profiles[player.id] = `https://picsum.photos/seed/${Math.random()
+          .toString(36)
+          .substring(2)}/500`;
+
+        return profiles;
+      })
+    );
+  }, [players]);
+
   return (
     <div id="players">
-      <span>players</span>
+      <div>
+        {placement.map((player) => (
+          <div key={player.id} style={{ top: player.placement * 5.75 + "rem" }}>
+            <span>{player.placement + 1}</span>
+            <img src={profiles[player.id]} />
+            <span>
+              {player.name}
+              {player.placement < 3 ? (
+                <i
+                  class={`fa-solid fa-medal placement-${player.placement + 1}`}
+                ></i>
+              ) : (
+                ""
+              )}
+            </span>
+            <span>
+              <i class="fa-solid fa-palette"></i> {player.score}
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
