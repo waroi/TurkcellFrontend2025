9f9552a667581a7fd365d93b1c5a8135e5818f87
Hikmet Melik Fırat <hikmetmelik@gmail.com>
2025-04-12 23:35:18 +0300

    Add Footer component and integrate it into RootLayoutProvider; update package.json to include lucide-react

--- Ogrenciler/Hikmet/odevler/final/src/components/RootLayoutProvider.tsx
+++ Ogrenciler/Hikmet/odevler/final/src/components/RootLayoutProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { SSRProvider } from "react-bootstrap";
+import Footer from "./landing-page/footer/Footer";
 import NavBar from "./landing-page/navbar/NavBar";
 
 export default function RootLayoutProvider({
@@ -12,6 +13,7 @@ export default function RootLayoutProvider({
 		<SSRProvider>
 			<NavBar />
 			{children}
+			<Footer />
 		</SSRProvider>
 	);
 }
---
+++ Ogrenciler/Hikmet/odevler/final/src/components/landing-page/footer/Footer.tsx
@@ -0,0 +1,205 @@
+import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react";
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+import Link from "next/link";
+import { Col, Container, Row } from "react-bootstrap";
+
+export default function Footer() {
+	const t = useTranslations("Footer");
+
+	return (
+		<>
+			<Container>
+				<footer className="py-5">
+					<Row className="g-4">
+						<Col xs={12} md={6} lg={3}>
+							<Image
+								src="/logo.svg"
+								alt="Logo"
+								width={100}
+								height={50}
+								className="mb-4"
+								loading="lazy"
+							/>
+							<h5 className="mb-3 fw-bold">{t("firstGrid.title")}</h5>
+							<p className="mb-1">{t("firstGrid.phoneNumber")}</p>
+							<p className="mb-3">{t("firstGrid.email")}</p>
+							<p className="text-muted small">{t("firstGrid.copyright")}</p>
+						</Col>
+
+						<Col xs={6} md={3} lg={2}>
+							<h6 className="mb-3 text-uppercase fw-bold">
+								{t("secondGrid.title")}
+							</h6>
+							<ul className="flex-column nav">
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("secondGrid.spot")}
+									</Link>
+								</li>
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("secondGrid.inverse")}
+									</Link>
+								</li>
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("secondGrid.perpetual")}
+									</Link>
+								</li>
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("secondGrid.exchange")}
+									</Link>
+								</li>
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("secondGrid.launchpad")}
+									</Link>
+								</li>
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("secondGrid.pay")}
+									</Link>
+								</li>
+							</ul>
+						</Col>
+
+						<Col xs={6} md={3} lg={2}>
+							<h6 className="mb-3 text-uppercase fw-bold">
+								{t("thirdGrid.title")}
+							</h6>
+							<ul className="flex-column nav">
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("thirdGrid.buyCrypto")}
+									</Link>
+								</li>
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("thirdGrid.markets")}
+									</Link>
+								</li>
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("thirdGrid.trading")}
+									</Link>
+								</li>
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("thirdGrid.affiliate")}
+									</Link>
+								</li>
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("thirdGrid.referral")}
+									</Link>
+								</li>
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("thirdGrid.api")}
+									</Link>
+								</li>
+							</ul>
+						</Col>
+
+						<Col xs={6} md={3} lg={2}>
+							<h6 className="mb-3 text-uppercase fw-bold">
+								{t("fourthGrid.title")}
+							</h6>
+							<ul className="flex-column nav">
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("fourthGrid.bybitLearn")}
+									</Link>
+								</li>
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("fourthGrid.helpCenter")}
+									</Link>
+								</li>
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("fourthGrid.userFeedback")}
+									</Link>
+								</li>
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("fourthGrid.submitRequest")}
+									</Link>
+								</li>
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("fourthGrid.apiDoc")}
+									</Link>
+								</li>
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("fourthGrid.tradingRules")}
+									</Link>
+								</li>
+							</ul>
+						</Col>
+
+						<Col xs={6} md={3} lg={3}>
+							<h6 className="mb-3 text-uppercase fw-bold">
+								{t("fifthGrid.title")}
+							</h6>
+							<ul className="flex-column nav">
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("fifthGrid.aboutBybit")}
+									</Link>
+								</li>
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("fifthGrid.authenticityCheck")}
+									</Link>
+								</li>
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("fifthGrid.careers")}
+									</Link>
+								</li>
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("fifthGrid.businessContacts")}
+									</Link>
+								</li>
+								<li className="mb-2 nav-item">
+									<Link href="#" className="p-0 text-muted nav-link">
+										{t("fifthGrid.blog")}
+									</Link>
+								</li>
+							</ul>
+						</Col>
+					</Row>
+				</footer>
+			</Container>
+			<div className="bg-body-tertiary">
+				<Container>
+					<Row className="d-flex flex-row align-items-center justify-content-between py-2">
+						<Col className="text-muted small">{t("firstGrid.copyright")}</Col>
+
+						<Col xs="auto">
+							<div className="d-flex gap-3">
+								<Link href="#" aria-label="Facebook" className="text-muted">
+									<Facebook />
+								</Link>
+								<Link href="#" aria-label="Twitter/X" className="text-muted">
+									<Twitter />
+								</Link>
+								<Link href="#" aria-label="Instagram" className="text-muted">
+									<Instagram />
+								</Link>
+								<Link href="#" aria-label="LinkedIn" className="text-muted">
+									<Linkedin />
+								</Link>
+							</div>
+						</Col>
+					</Row>
+				</Container>
+			</div>
+		</>
+	);
+}
