533ce3e1f315dd8a95c3b2f384ed8abdc6f08c6b
beyzasaridas <128313968+beyzasaridas@users.noreply.github.com>
2025-04-17 18:59:39 +0300

    update

--- Ogrenciler/BeyzaSaridas/final_ödev/crypto-exchange/src/components/layout/Footer.tsx
+++ Ogrenciler/BeyzaSaridas/final_ödev/crypto-exchange/src/components/layout/Footer.tsx
@@ -19,11 +19,16 @@ const Footer: React.FC = () => {
               <svg className="footer__logo-icon" viewBox="0 0 24 24">
                 <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
               </svg>
-              <span className="footer__logo-text">Crypto Planet</span>
+              <span className="footer__logo-text">Rocket</span>
             </div>
             <p className="footer__description">
-              Modern ve güvenli kripto para ticaret platformu. Tüm kripto
-              ihtiyaçlarınız için tek adres.
+              <strong>Let's talk! 🤙</strong>
+              <br /><br />
+              +98 902 353 2926
+              <br /><br />
+              saridas.beyzaa@gmail.com
+              <br /><br />
+              Copyright © 2023 Free For Earth’s people
             </p>
             <div className="footer__social">
               <a href="#" className="footer__social-link" aria-label="Twitter">
@@ -31,7 +36,7 @@ const Footer: React.FC = () => {
                   <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
                 </svg>
               </a>
-              <a href="#" className="footer__social-link" aria-label="GitHub">
+              <a href="https://github.com/beyzasaridas" className="footer__social-link" aria-label="GitHub">
                 <svg viewBox="0 0 24 24" className="footer__social-icon">
                   <path
                     fillRule="evenodd"
@@ -40,7 +45,7 @@ const Footer: React.FC = () => {
                   ></path>
                 </svg>
               </a>
-              <a href="#" className="footer__social-link" aria-label="LinkedIn">
+              <a href="https://www.linkedin.com/in/beyza-saridas/" className="footer__social-link" aria-label="LinkedIn">
                 <svg viewBox="0 0 24 24" className="footer__social-icon">
                   <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
                 </svg>
@@ -129,7 +134,7 @@ const Footer: React.FC = () => {
 
         <div className="footer__bottom">
           <p className="footer__copyright">
-            &copy; {currentYear} Crypto Planet. {t("footer.allRightsReserved")}
+            &copy; Copyright 2023 Free For World People
           </p>
         </div>
       </div>
--- Ogrenciler/BeyzaSaridas/final_ödev/crypto-exchange/src/components/layout/Header.tsx
+++ Ogrenciler/BeyzaSaridas/final_ödev/crypto-exchange/src/components/layout/Header.tsx
@@ -55,7 +55,7 @@ const Header: React.FC = () => {
                 <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
               </svg>
               <Link href="/" className="header__logo-text">
-                Crypto Planet
+                Rocket
               </Link>
             </div>
 
