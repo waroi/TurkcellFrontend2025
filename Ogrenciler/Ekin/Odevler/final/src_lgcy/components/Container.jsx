74498330f31bd69190e66d6d6ce9db6b28bde56a
Ekin Aslan <ekinaslan.js@gmail.com>
2025-04-21 17:34:04 +0300

    Reset

--- Ogrenciler/Ekin/Odevler/final/src_lgcy/components/Container.jsx
+++
@@ -1,7 +0,0 @@
-export default function Container({ className, children }) {
-  return (
-    <section className={className}>
-      <div className="container">{children}</div>
-    </section>
-  );
-}
